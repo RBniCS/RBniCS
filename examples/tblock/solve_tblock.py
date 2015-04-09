e1df258359c286939d03174f87621daa2aa8e950
Francesco Ballarin <francesco.ballarin@sissa.it>
2015-04-09 13:36:45 +0200

    Update the tblock example in agreement with the two previous commits

--- examples/tblock/solve_tblock.py
+++ examples/tblock/solve_tblock.py
@@ -15,89 +15,100 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
 #
+## @file solve_tblock.py
+#  @brief Example 1: thermal block test case
+#
+#  @author Francesco Ballarin <francesco.ballarin@sissa.it>
+#  @author Gianluigi Rozza    <gianluigi.rozza@sissa.it>
+#  @author Alberto   Sartori  <alberto.sartori@sissa.it>
+
 from dolfin import *
 from elliptic_coercive_base import *
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~     EXAMPLE 1: THERMAL BLOCK CLASS     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
 class Tblock(EllipticCoerciveBase):
-
+    
+    ## Default initialization of members
+    def __init__(self, V, subd, bound):
+        EllipticCoerciveBase.__init__(self, V)
+        self.dx = Measure("dx")[subd]
+        self.ds = Measure("ds")[bound]
+        self.bc = DirichletBC(V, 0.0, bound, 3)
+    
+    ## Return the alpha_lower bound.
     def get_alpha_lb(self):
         return 0.1
-
+    
+    ## Set theta multiplicative terms of the affine expansion of a.
     def compute_theta_a(self):
         mu1 = self.mu[0]
         mu2 = self.mu[1]
         theta_a0 = mu1
         theta_a1 = 1.
         self.theta_a = (theta_a0, theta_a1)
-
+    
+    ## Set theta multiplicative terms of the affine expansion of f.
     def compute_theta_f(self):
         self.theta_f = (self.mu[1],)
+    
+    ## Set matrices resulting from the truth discretization of a.
+    def compute_truth_a(self):
+        u = self.u
+        v = self.v
+        dx = self.dx
+        # Assemble A0
+        a0 = inner(grad(u),grad(v))*dx(1) +1e-15*inner(u,v)*dx
+        A0 = assemble(a0)
+        # Assemble A1
+        a1 = inner(grad(u),grad(v))*dx(2) +1e-15*inner(u,v)*dx
+        A1 = assemble(a1)
+        # Apply BCs
+        self.bc.apply(A0)
+        self.bc.apply(A1)
+        self.bc.zero(A1)
+        # Save
+        self.A_vec = (A0, A1)
+    
+    ## Set vectors resulting from the truth discretization of f.
+    def compute_truth_f(self):
+        v = self.v
+        dx = self.dx
+        ds = self.ds
+        # Assemble F0
+        f0 = v*ds(1) + 1e-15*v*dx
+        F0 = assemble(f0)
+        # Apply BCs
+        self.bc.apply(F0)
+        # Save
+        self.F_vec = (F0,)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~     EXAMPLE 1: MAIN PROGRAM     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
 
-parameters.linear_algebra_backend = 'PETSc'
+# 1. Read the mesh for this problem
+mesh = Mesh("data/tblock.xml")
+subd = MeshFunction("size_t", mesh, "data/tblock_physical_region.xml")
+bound = MeshFunction("size_t", mesh, "data/tblock_facet_region.xml")
 
-# Create mesh and define function space
-mesh = Mesh("tblock.xml")
-subd = MeshFunction("size_t", mesh, "tblock_physical_region.xml")
-bound = MeshFunction("size_t", mesh, "tblock_facet_region.xml")
-
-
-
+# 2. Create Finite Element space (Lagrange P1)
 V = FunctionSpace(mesh, "Lagrange", 1)
 
-tb = Tblock(V)
+# 3. Allocate an object of the Thermal Block class
+tb = Tblock(V, subd, bound)
 
-# 
-# Define new measures associated with the interior domains and
-# exterior boundaries
-dx = Measure("dx")[subd]
-ds = Measure("ds")[bound]
-# 
-# mu1 = 0.3
-# mu2 = 2.0
-# mur = 2.0
-# mu3 = 5.0
-# 
-# 
-# 
-# Define variational problem
-u = TrialFunction(V)
-v = TestFunction(V)
+# 4. Choose PETSc solvers as linear algebra backend
+parameters.linear_algebra_backend = 'PETSc'
 
-a0 = inner(grad(u),grad(v))*dx(1) +1e-15*inner(u,v)*dx
-A0 = assemble(a0)
-
-a1 = inner(grad(u),grad(v))*dx(2) +1e-15*inner(u,v)*dx
-A1 = assemble(a1)
-
-
-
-f0 = v*ds(1) + 1e-15*v*dx
-F0 = assemble(f0)
-out0 = v*ds(1)
-Out0 = assemble(out0)
-
-bc = DirichletBC(V, 0.0, bound, 3)
-
-A_vec_no_bc = (A0, A1)
-bc.apply(A0)
-bc.apply(A1)
-bc.zero(A1)
-bc.apply(F0)
-
-
-A_vec = (A0, A1)
-F_vec = (F0,)
-
-tb.setA_vec(A_vec)
-tb.setF_vec(F_vec)
-
+# 5. Set mu range, eta_train and Nmax
 mu_range = [(0.1, 10.0), (-1.0, 1.0)]
 tb.setmu_range(mu_range)
-tb.settheta_train(1000)
+tb.seteta_train(1000)
 tb.setNmax(4)
+
+# 6. Perform the offline phase
 first_mu = (0.5,1.0)
 tb.setmu(first_mu)
-#tb.offline()
+tb.offline()
+
+# 7. Perform an online solve
 mu = (0.3,-1.0)
 tb.online_solve(mu)
