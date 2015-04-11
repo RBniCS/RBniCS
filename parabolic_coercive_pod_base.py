b83bf094eba726ea1d7332e700e9dc1b6599b207
Francesco Ballarin <francesco.ballarin@sissa.it>
2015-04-11 16:17:49 +0200

    Add parabolic POD class

---
+++ parabolic_coercive_pod_base.py
@@ -0,0 +1,72 @@
+# Copyright (C) 2015 SISSA mathLab
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+## @file parabolic_coercive_pod_base.py
+#  @brief Implementation of a POD-Galerkin ROM for parabolic coervice problems
+#
+#  @author Francesco Ballarin <francesco.ballarin@sissa.it>
+#  @author Gianluigi Rozza    <gianluigi.rozza@sissa.it>
+#  @author Alberto   Sartori  <alberto.sartori@sissa.it>
+
+from parabolic_coercive_base import *
+from elliptic_coercive_pod_base import *
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~     PARABOLIC COERCIVE POD BASE CLASS     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
+## @class ParabolicCoercivePODBase
+#
+# Base class containing the interface of a POD-Galerkin ROM
+# for parabolic coercive problems
+class ParabolicCoercivePODBase(ParabolicCoerciveBase,EllipticCoercivePODBase):
+# Beware of the diamond problem in multiple inheritance: in python precedence is depth-first and then left-to-right
+
+    ###########################     CONSTRUCTORS     ########################### 
+    ## @defgroup Constructors Methods related to the construction of the POD-Galerkin ROM object
+    #  @{
+    
+    ## Default initialization of members
+    def __init__(self, V):
+        # Call the parent initialization
+        ParabolicCoerciveBase.__init__(self, V)
+        EllipticCoercivePODBase.__init__(self, V)
+        
+    #  @}
+    ########################### end - CONSTRUCTORS - end ########################### 
+    
+    ###########################     ONLINE STAGE     ########################### 
+    ## @defgroup OnlineStage Methods related to the online stage
+    #  @{
+    
+    # Nothing to be added in this case
+        
+    #  @}
+    ########################### end - ONLINE STAGE - end ########################### 
+    
+    ###########################     OFFLINE STAGE     ########################### 
+    ## @defgroup OfflineStage Methods related to the offline stage
+    #  @{
+
+    ## Update the snapshot matrix
+    def update_snapshot_matrix(self):
+        if self.snapshot_matrix.size == 0: # for the first snapshot
+            self.snapshot_matrix = np.array(self.snap) # as column vectors
+        else:
+            self.snapshot_matrix = np.hstack((self.snapshot_matrix, self.snap)) # add new snapshots as column vectors
+            
+    
+    #  @}
+    ########################### end - OFFLINE STAGE - end ########################### 
+
