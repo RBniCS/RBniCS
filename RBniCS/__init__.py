6598456a980179c7c4cbd093c2fa39512705f648
alberto sartori <alberto.sartori86@gmail.com>
2016-03-14 23:01:43 +0100

    documented __init__.py

--- RBniCS/__init__.py
+++ RBniCS/__init__.py
@@ -23,20 +23,70 @@ __license__ = "LGPL"
 __version__ = "0.0.1"
 __email__ = "francesco.ballarin@sissa.it, gianluigi.rozza@sissa.it, alberto.sartori@sissa.it"
 
+# Define the __init__.py file. The following instructions are executed
+# when the import of RBniCS is done.
 
-from eim import *
+# This is the base class, which is inherited by all other classes. It
+# defines the base interface with variables and functions that the
+# derived classes have to set and/or overwrite.
+from parametrized_problem import *
+
+# The first kind of problem to be derived from the class
+# ParametrizedProblem is the class EllipticCorciveBase. This class
+# defines and implement variables and methods that needed for solving
+# an elliptic and coercive problem. This class specializes in the two
+# currently implemented reduced order methods, namely the Reduced
+# Basis Method (EllipticCoerciveRBBase), and the Proper Orthogonal
+# Decomposition (EllipticCoercivePODBase). These two classes assume
+# that the output(s) of interest is (are) compliant. Whether the
+# compliancy hypothesis does not hold, the
+# EllipticCoerciveRBNonCompliantBase must be used.
 from elliptic_coercive_base import *
-from elliptic_coercive_pod_base import *
+
+# Compliant case
 from elliptic_coercive_rb_base import *
+from elliptic_coercive_pod_base import *
+
+# Non compliant case
 from elliptic_coercive_rb_non_compliant_base import *
-from gram_schmidt import *
+
+# The common interface for handling time dependent problem is
+# implemented in the ParabolicCoerciveBase, which is derived from
+# EllipticCoerciveBase. Indeed, at each temporal step, and elliptic
+# problem is solved. As for the elliptic case, this class is
+# specialized for both the Reduced Basis method as well as for the
+# Proper Orthogonal Decomposition. In both case, the compliancy is assumed to hold.
 from parabolic_coercive_base import *
+
+# Compliant case
 from parabolic_coercive_pod_base import *
 from parabolic_coercive_rb_base import *
-from parametrized_problem import *
+
+
+#### Helper functions and classes
+
+# This class implements the Gram Schmidt orthogonalization
+from gram_schmidt import *
+
+# This class provides the methods needed to perform the Proper Orthogonal Decomposition
 from proper_orthogonal_decomposition import *
+
+# This class is dedicated to the implementation of the Successive
+# Constraint Method (SCM), which is used within the Reduced Basis
+# approach in order to have an estimation of the lower bound of the
+# coercivty constant. Such lower bound enters the a posteriori error
+# estimation.
 from scm import *
+
+# If the geometry is parametrized, this class takes care of producing
+# nice plots where the mesh is properly deformed.
 from shape_parametrization import *
+
+# In this class the Empirical interpolation Method (EIM) is coded,
+# which is a standard procedure which leads to an approximate affine
+# decomposition when the problem does not admit one.
+from eim import *
+
 
 __all__ = [ \
               'EIM', \
