36f3a5550a1cbee68c7637b873462bc9ac95cb34
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-10-05 11:34:14 +0200

    Duplicate tutorial 5 script (EIM vs DEIM) and add the approximation of a Gaussian also as unit test

---
+++ tests/methodology/eim/test_eim_approximation_3.py
@@ -0,0 +1,84 @@
+# Copyright (C) 2015-2016 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+## @file 
+#  @brief 
+#
+#  @author Francesco Ballarin <francesco.ballarin@sissa.it>
+#  @author Gianluigi Rozza    <gianluigi.rozza@sissa.it>
+#  @author Alberto   Sartori  <alberto.sartori@sissa.it>
+
+from dolfin import *
+from RBniCS import EquispacedDistribution, ParametrizedExpression
+from RBniCS.backends import ProjectedParametrizedExpression, ProjectedParametrizedTensor
+from RBniCS.eim.problems.eim_approximation import EIMApproximation
+from RBniCS.eim.reduction_methods.eim_approximation_reduction_method import EIMApproximationReductionMethod
+
+class ParametrizedFunctionApproximation(EIMApproximation):
+    def __init__(self, V, expression_type, basis_generation):
+        # Parametrized function to be interpolated
+        f = ParametrizedExpression(self, "exp( - 2*pow(x[0]-mu[0], 2) - 2*pow(x[1]-mu[1], 2) )", mu=(0., 0.), element=V.ufl_element())
+        #
+        assert expression_type in ("Function", "Vector", "Matrix")
+        if expression_type == "Function":
+            # Call Parent constructor
+            EIMApproximation.__init__(self, None, ProjectedParametrizedExpression(f, V), "test_eim_approximation_3_function.output_dir", basis_generation)
+        elif expression_type == "Vector":
+            v = TestFunction(V)
+            form = f*v*dx
+            # Call Parent constructor
+            EIMApproximation.__init__(self, None, ProjectedParametrizedTensor(form, V), "test_eim_approximation_3_vector.output_dir", basis_generation)
+        elif expression_type == "Matrix":
+            u = TrialFunction(V)
+            v = TestFunction(V)
+            form = f*u*v*dx
+            # Call Parent constructor
+            EIMApproximation.__init__(self, None, ProjectedParametrizedTensor(form, V), "test_eim_approximation_3_matrix.output_dir", basis_generation)
+        else: # impossible to arrive here anyway thanks to the assert
+            raise AssertionError("Invalid expression_type")
+
+# 1. Create the mesh for this test
+mesh = Mesh("../../../tutorials/05_gaussian/data/gaussian.xml")
+subdomains = MeshFunction("size_t", mesh, "../../../tutorials/05_gaussian/data/gaussian_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "../../../tutorials/05_gaussian/data/gaussian_facet_region.xml")
+
+# 2. Create Finite Element space (Lagrange P1)
+V = FunctionSpace(mesh, "Lagrange", 1)
+
+# 3. Allocate an object of the ParametrizedFunctionApproximation class
+expression_type = "Function" # Function or Vector or Matrix
+basis_generation = "Greedy" # Greedy or POD
+parametrized_function_approximation = ParametrizedFunctionApproximation(V, expression_type, basis_generation)
+mu_range = [(-1.0, 1.0), (-1.0, 1.0)]
+parametrized_function_approximation.set_mu_range(mu_range)
+
+# 4. Prepare reduction with EIM
+parametrized_function_reduction_method = EIMApproximationReductionMethod(parametrized_function_approximation)
+parametrized_function_reduction_method.set_Nmax(20)
+
+# 5. Perform the offline phase
+parametrized_function_reduction_method.set_xi_train(100, sampling=EquispacedDistribution())
+reduced_parametrized_function_approximation = parametrized_function_reduction_method.offline()
+
+# 6. Perform an online solve
+online_mu = (-1., -1.)
+reduced_parametrized_function_approximation.set_mu(online_mu)
+reduced_parametrized_function_approximation.solve()
+
+# 7. Perform an error analysis
+parametrized_function_reduction_method.set_xi_test(100)
+parametrized_function_reduction_method.error_analysis()
---
+++ tutorials/05_gaussian/solve_gaussian_deim.py
@@ -0,0 +1,149 @@
+# Copyright (C) 2015-2016 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+## @file solve_gaussian.py
+#  @brief Example 5: gaussian EIM test case
+#
+#  @author Francesco Ballarin <francesco.ballarin@sissa.it>
+#  @author Gianluigi Rozza    <gianluigi.rozza@sissa.it>
+#  @author Alberto   Sartori  <alberto.sartori@sissa.it>
+
+from dolfin import *
+from RBniCS import *
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~     EXAMPLE 5: GAUSSIAN DEIM CLASS     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
+@DEIM()
+class Gaussian(EllipticCoerciveProblem):
+    
+    ###########################     CONSTRUCTORS     ########################### 
+    ## @defgroup Constructors Methods related to the construction of the reduced order model object
+    #  @{
+    
+    ## Default initialization of members
+    def __init__(self, V, **kwargs):
+        # Call the standard initialization
+        EllipticCoerciveProblem.__init__(self, V, **kwargs)
+        # ... and also store FEniCS data structures for assembly
+        assert "subdomains" in kwargs
+        assert "boundaries" in kwargs
+        self.subdomains, self.boundaries = kwargs["subdomains"], kwargs["boundaries"]
+        self.u = TrialFunction(V)
+        self.v = TestFunction(V)
+        self.dx = dx #Measure("dx")(subdomain_data=subdomains)
+        self.f = ParametrizedExpression(self, "exp( - 2*pow(x[0]-mu[0], 2) - 2*pow(x[1]-mu[1], 2) )", mu=(0., 0.), element=V.ufl_element())
+        # note that we cannot use self.mu in the initialization of self.f, because self.mu has not been initialized yet
+        
+    #  @}
+    ########################### end - CONSTRUCTORS - end ########################### 
+    
+    ###########################     PROBLEM SPECIFIC     ########################### 
+    ## @defgroup ProblemSpecific Problem specific methods
+    #  @{
+    
+    ## Return the alpha_lower bound.
+    def get_stability_factor(self):
+        return 1.
+    
+    ## Return theta multiplicative terms of the affine expansion of the problem.
+    def compute_theta(self, term):
+        if term == "a":
+            return (1., )
+        elif term == "f":
+            return (1., )
+        else:
+            raise ValueError("Invalid term for compute_theta().")
+                
+    ## Return forms resulting from the discretization of the affine expansion of the problem operators.
+    def assemble_operator(self, term):
+        v = self.v
+        dx = self.dx
+        if term == "a":
+            u = self.u
+            a0 = inner(grad(u),grad(v))*dx
+            return (a0,)
+        elif term == "f":
+            f = self.f
+            f0 = f*v*dx
+            return (f0,)
+        elif term == "dirichlet_bc":
+            bc0 = [DirichletBC(self.V, Constant(0.0), self.boundaries, 1),
+                   DirichletBC(self.V, Constant(0.0), self.boundaries, 2),
+                   DirichletBC(self.V, Constant(0.0), self.boundaries, 3)]
+            return (bc0,)
+        elif term == "inner_product":
+            u = self.u
+            x0 = inner(grad(u),grad(v))*dx
+            return (x0,)
+        else:
+            raise ValueError("Invalid term for assemble_operator().")
+            
+    #  @}
+    ########################### end - PROBLEM SPECIFIC - end ########################### 
+    
+#~~~~~~~~~~~~~~~~~~~~~~~~~     EXAMPLE 5: MAIN PROGRAM     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
+
+# 1. Read the mesh for this problem
+mesh = Mesh("data/gaussian.xml")
+subdomains = MeshFunction("size_t", mesh, "data/gaussian_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "data/gaussian_facet_region.xml")
+
+# 2. Create Finite Element space (Lagrange P1)
+V = FunctionSpace(mesh, "Lagrange", 1)
+
+# 3. Allocate an object of the Gaussian class
+gaussian_problem = Gaussian(V, subdomains=subdomains, boundaries=boundaries)
+mu_range = [(-1.0, 1.0), (-1.0, 1.0)]
+gaussian_problem.set_mu_range(mu_range)
+
+# 4. Prepare reduction with a reduced basis method
+reduced_basis_method = ReducedBasis(gaussian_problem)
+reduced_basis_method.set_Nmax(20, DEIM=21)
+
+# 5. Perform the offline phase
+first_mu = (0.5,1.0)
+gaussian_problem.set_mu(first_mu)
+reduced_basis_method.set_xi_train(50)
+reduced_gaussian_problem = reduced_basis_method.offline()
+
+# 6. Perform an online solve
+online_mu = (0.3,-1.0)
+reduced_gaussian_problem.set_mu(online_mu)
+reduced_gaussian_problem.solve()
+reduced_gaussian_problem.export_solution("Gaussian", "online_solution")
+reduced_gaussian_problem.solve(DEIM=11)
+reduced_gaussian_problem.export_solution("Gaussian", "online_solution__DEIM_11")
+reduced_gaussian_problem.solve(DEIM=1)
+reduced_gaussian_problem.export_solution("Gaussian", "online_solution__DEIM_1")
+
+# 7. Perform an error analysis
+reduced_basis_method.set_xi_test(50)
+reduced_basis_method.error_analysis()
+
+# 8. Define a new class corresponding to the exact version of Gaussian,
+#    for which DEIM is replaced by ExactParametrizedFunctions
+ExactGaussian = ExactProblem(Gaussian)
+
+# 9. Allocate an object of the ExactGaussian class
+exact_gaussian_problem = ExactGaussian(V, subdomains=subdomains, boundaries=boundaries)
+exact_gaussian_problem.set_mu_range(mu_range)
+
+# 10. Perform an error analysis with respect to the exact problem
+reduced_basis_method.error_analysis(with_respect_to=exact_gaussian_problem)
+
+# 11. Perform an error analysis with respect to the exact problem, but
+#     employing a smaller number of DEIM basis functions
+reduced_basis_method.error_analysis(with_respect_to=exact_gaussian_problem, DEIM=11)
