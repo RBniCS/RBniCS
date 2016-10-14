c0d541c9f5c66c09147ff5aa8b8362423ec3af38
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-10-14 15:59:36 +0200

    Simplify FEniCS projected parametrized expression: there is no need to reconstruct an UFL element for the function space, since expression had already been provided with one

--- RBniCS/backends/fenics/projected_parametrized_expression.py
+++ RBniCS/backends/fenics/projected_parametrized_expression.py
@@ -36,41 +36,8 @@ from RBniCS.utils.mpi import parallel_max
 class ProjectedParametrizedExpression(AbstractProjectedParametrizedExpression):
     def __init__(self, expression, original_space):
         AbstractProjectedParametrizedExpression.__init__(self, expression, original_space)
-        #
         self._expression = expression
-        shape = expression.ufl_shape
-        sub_elements_queue = original_space.ufl_element().sub_elements()
-        if len(sub_elements_queue) > 0:
-            sub_elements_queue_index = 0
-            scalar_sub_elements = list()
-            while sub_elements_queue_index < len(sub_elements_queue):
-                current_sub_elements = sub_elements_queue[sub_elements_queue_index].sub_elements()
-                if len(current_sub_elements) > 0:
-                    for current_sub_element in current_sub_elements:
-                        sub_elements_queue.append(current_sub_element)
-                else:
-                    scalar_sub_elements.append(sub_elements_queue[sub_elements_queue_index])
-                sub_elements_queue_index += 1
-            assert len(scalar_sub_elements) > 0
-            first_family = scalar_sub_elements[0].family()
-            max_degree = scalar_sub_elements[0].degree()
-            scalar_element = scalar_sub_elements[0]
-            for scalar_sub_element in scalar_sub_elements:
-                assert scalar_sub_element.family() == first_family
-                if scalar_sub_element.degree() > max_degree:
-                    scalar_element = scalar_sub_element
-        else:
-            scalar_element = original_space.ufl_element()
-        assert len(shape) in (0, 1, 2)
-        if len(shape) == 0:
-            expression_element = scalar_element
-        elif len(shape) == 1:
-            expression_element = VectorElement(scalar_element)
-        elif len(shape) == 2:
-            expression_element = TensorElement(scalar_element)
-        else:
-            raise AssertionError("Invalid function space in ProjectedParametrizedExpression")
-        self._space = FunctionSpace(original_space.mesh(), expression_element)
+        self._space = FunctionSpace(original_space.mesh(), expression.ufl_element())
             
     @override
     def create_interpolation_locations_container(self):
