08bf043181b068cd0bd0858a7f8a28903451677b
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-11-04 18:30:49 +0100

    Better guard IDA in numpy timestepping

--- RBniCS/backends/numpy/time_stepping.py
+++ RBniCS/backends/numpy/time_stepping.py
@@ -27,7 +27,7 @@ from numpy import arange, asarray, linspace
 try:
     from assimulo.solvers import IDA
     from assimulo.problem import Implicit_Problem
-except:
+except ImportError:
     has_IDA = False
 else:
     has_IDA = True
@@ -226,127 +226,128 @@ class _ScipyImplicitEuler(object):
         
         return all_solutions
         
-class _AssimuloIDA(object):
-    def __init__(self, residual_eval, solution, bc_eval, jacobian_eval):
-        self.residual_eval = residual_eval
-        self.solution = solution
-        self.solution_dot = Function(solution.vector().N) # equal to zero
-        self.bc_eval = bc_eval
-        self.jacobian_eval = jacobian_eval
-        # We should be solving a square system
-        self.sample_residual = residual_eval(0, self.solution.vector(), self.solution_dot.vector())
-        self.sample_jacobian = jacobian_eval(0, self.solution.vector(), self.solution_dot.vector(), 0.)
-        assert self.sample_jacobian.M == self.sample_jacobian.N
-        assert self.sample_jacobian.N == self.sample_residual.N
-        # Storage for current BC
-        self.current_bc = None
-        # Define an Assimulo Implicit problem
-        def _store_solution_and_solution_dot(t, solution, solution_dot):
-            self.solution.vector()[:] = solution.reshape((-1, 1))
-            self.solution_dot.vector()[:] = solution_dot.reshape((-1, 1))
-            # Update current bc
-            if self.bc_eval is not None:
-                self.current_bc = DirichletBC(self.sample_jacobian, self.sample_residual, self.bc_eval(t))
-                self.current_bc.apply_to_vector(self.solution.vector())
-                solution[:] = asarray(self.solution.vector()).reshape(-1)
-        def _assimulo_residual_eval(t, solution, solution_dot):
-            # Convert to a matrix with one column, rather than an array
-            _store_solution_and_solution_dot(t, solution, solution_dot)
-            # Compute residual
-            residual_vector = self.residual_eval(t, solution, solution_dot)
-            # Apply BCs, if necessary
-            if self.bc_eval is not None:
-                self.current_bc.homogeneous_apply_to_vector(residual_vector)
-            # Convert to an array, rather than a matrix with one column, and return
-            return asarray(residual_vector).reshape(-1)
-        def _assimulo_jacobian_eval(solution_dot_coefficient, t, solution, solution_dot):
-            # Convert to a matrix with one column, rather than an array
-            _store_solution_and_solution_dot(t, solution, solution_dot)
-            # Compute jacobian
-            jacobian_matrix = self.jacobian_eval(t, self.solution.vector(), self.solution_dot.vector(), solution_dot_coefficient)
-            # Apply BCs, if necessary
-            if self.bc_eval is not None:
-                self.current_bc.apply_to_matrix(jacobian_matrix)
-            # Return
-            return jacobian_matrix
-        self.problem = Implicit_Problem(_assimulo_residual_eval, self.solution.vector(), self.solution_dot.vector())
-        self.problem.jac = _assimulo_jacobian_eval
-        # Define an Assimulo IDA solver
-        self.solver = IDA(self.problem)
-        self.solver.display_progress = False
-        # Additional storage which will be setup by set_parameters
-        self._final_time = None
-        self._initial_time = 0.
-        self._max_time_steps = None
-        self._monitor = None
-        self._time_step_size = None
-        
-    def set_parameters(self, parameters):
-        for (key, value) in parameters.iteritems():
-            if key == "absolute_tolerance":
-                self.solver.atol = value
-            elif key == "final_time":
-                self._final_time = value
-            elif key == "initial_time":
-                assert value == 0
-            elif key == "integrator_type":
-                assert value == "ida"
-            elif key == "max_time_steps":
-                self.solver.maxsteps = value
-                self._max_time_steps = value
-            elif key == "monitor":
-                self._monitor = value
-            elif key == "nonlinear_solver":
-                for (key_nonlinear, value_nonlinear) in value.iteritems():
-                    if key_nonlinear == "absolute_tolerance":
-                        self.solver.atol = value_nonlinear
-                    elif key_nonlinear == "line_search":
-                        raise NotImplementedError("This feature has not been implemented in IDA.")
-                    elif key_nonlinear == "maximum_iterations":
-                        raise NotImplementedError("This feature has not been implemented in IDA.")
-                    elif key_nonlinear == "relative_tolerance":
-                        self.solver.rtol = value
-                    elif key_nonlinear == "report":
-                        raise NotImplementedError("This feature has not been implemented in IDA.")
-                    elif key_nonlinear == "solution_tolerance":
-                        raise NotImplementedError("This feature has not been implemented in IDA.")
-                    else:
-                        raise ValueError("Invalid paramater passed to _AssimuloIDA object.")
-            elif key == "problem_type":
-                pass
-            elif key == "relative_tolerance":
-                self.solver.rtol = value
-            elif key == "report":
-                self.solver.display_progress = True
-                self.solver.report_continuously = True
-            elif key == "time_step_size":
-                self.solver.inith = value
-                self._time_step_size = value
-            else:
-                raise ValueError("Invalid paramater passed to _AssimuloIDA object.")
-        
-    def solve(self):
-        assert self._max_time_steps is not None or self._time_step_size is not None
-        if self._time_step_size is not None:
-            all_t = [0]
-            all_t_arange = self._time_step_size + arange(self._initial_time, self._final_time, self._time_step_size)
-            all_t.extend(all_t_arange.tolist())
-        elif self._max_time_steps is not None:
-            all_t = linspace(self._initial_time, self._final_time, num=self._max_time_steps+1)
-            all_t = all_t.tolist()
-        all_times, all_solutions, all_solutions_dot = self.solver.simulate(self._final_time, ncp_list=all_t)
-        # Convert all_solutions to a list of Function
-        all_solutions_as_functions = list()
-        for (t, solution) in zip(all_times, all_solutions):
-            solution_as_function = Function(self.solution.vector().N)
-            solution_as_function.vector()[:] = solution.reshape((-1, 1))
-            # Fix bcs
-            if self.bc_eval is not None:
-                self.current_bc = DirichletBC(self.sample_jacobian, self.sample_residual, self.bc_eval(t))
-                self.current_bc.apply_to_vector(solution_as_function.vector())
-            all_solutions_as_functions.append(solution_as_function)
-            if self._monitor is not None:
-                self._monitor(t, solution_as_function.vector())
-        self.solution.vector()[:] = all_solutions_as_functions[-1].vector()
-        return all_solutions_as_functions
-        
+if has_IDA:
+    class _AssimuloIDA(object):
+        def __init__(self, residual_eval, solution, bc_eval, jacobian_eval):
+            self.residual_eval = residual_eval
+            self.solution = solution
+            self.solution_dot = Function(solution.vector().N) # equal to zero
+            self.bc_eval = bc_eval
+            self.jacobian_eval = jacobian_eval
+            # We should be solving a square system
+            self.sample_residual = residual_eval(0, self.solution.vector(), self.solution_dot.vector())
+            self.sample_jacobian = jacobian_eval(0, self.solution.vector(), self.solution_dot.vector(), 0.)
+            assert self.sample_jacobian.M == self.sample_jacobian.N
+            assert self.sample_jacobian.N == self.sample_residual.N
+            # Storage for current BC
+            self.current_bc = None
+            # Define an Assimulo Implicit problem
+            def _store_solution_and_solution_dot(t, solution, solution_dot):
+                self.solution.vector()[:] = solution.reshape((-1, 1))
+                self.solution_dot.vector()[:] = solution_dot.reshape((-1, 1))
+                # Update current bc
+                if self.bc_eval is not None:
+                    self.current_bc = DirichletBC(self.sample_jacobian, self.sample_residual, self.bc_eval(t))
+                    self.current_bc.apply_to_vector(self.solution.vector())
+                    solution[:] = asarray(self.solution.vector()).reshape(-1)
+            def _assimulo_residual_eval(t, solution, solution_dot):
+                # Convert to a matrix with one column, rather than an array
+                _store_solution_and_solution_dot(t, solution, solution_dot)
+                # Compute residual
+                residual_vector = self.residual_eval(t, solution, solution_dot)
+                # Apply BCs, if necessary
+                if self.bc_eval is not None:
+                    self.current_bc.homogeneous_apply_to_vector(residual_vector)
+                # Convert to an array, rather than a matrix with one column, and return
+                return asarray(residual_vector).reshape(-1)
+            def _assimulo_jacobian_eval(solution_dot_coefficient, t, solution, solution_dot):
+                # Convert to a matrix with one column, rather than an array
+                _store_solution_and_solution_dot(t, solution, solution_dot)
+                # Compute jacobian
+                jacobian_matrix = self.jacobian_eval(t, self.solution.vector(), self.solution_dot.vector(), solution_dot_coefficient)
+                # Apply BCs, if necessary
+                if self.bc_eval is not None:
+                    self.current_bc.apply_to_matrix(jacobian_matrix)
+                # Return
+                return jacobian_matrix
+            self.problem = Implicit_Problem(_assimulo_residual_eval, self.solution.vector(), self.solution_dot.vector())
+            self.problem.jac = _assimulo_jacobian_eval
+            # Define an Assimulo IDA solver
+            self.solver = IDA(self.problem)
+            self.solver.display_progress = False
+            # Additional storage which will be setup by set_parameters
+            self._final_time = None
+            self._initial_time = 0.
+            self._max_time_steps = None
+            self._monitor = None
+            self._time_step_size = None
+            
+        def set_parameters(self, parameters):
+            for (key, value) in parameters.iteritems():
+                if key == "absolute_tolerance":
+                    self.solver.atol = value
+                elif key == "final_time":
+                    self._final_time = value
+                elif key == "initial_time":
+                    assert value == 0
+                elif key == "integrator_type":
+                    assert value == "ida"
+                elif key == "max_time_steps":
+                    self.solver.maxsteps = value
+                    self._max_time_steps = value
+                elif key == "monitor":
+                    self._monitor = value
+                elif key == "nonlinear_solver":
+                    for (key_nonlinear, value_nonlinear) in value.iteritems():
+                        if key_nonlinear == "absolute_tolerance":
+                            self.solver.atol = value_nonlinear
+                        elif key_nonlinear == "line_search":
+                            raise NotImplementedError("This feature has not been implemented in IDA.")
+                        elif key_nonlinear == "maximum_iterations":
+                            raise NotImplementedError("This feature has not been implemented in IDA.")
+                        elif key_nonlinear == "relative_tolerance":
+                            self.solver.rtol = value
+                        elif key_nonlinear == "report":
+                            raise NotImplementedError("This feature has not been implemented in IDA.")
+                        elif key_nonlinear == "solution_tolerance":
+                            raise NotImplementedError("This feature has not been implemented in IDA.")
+                        else:
+                            raise ValueError("Invalid paramater passed to _AssimuloIDA object.")
+                elif key == "problem_type":
+                    pass
+                elif key == "relative_tolerance":
+                    self.solver.rtol = value
+                elif key == "report":
+                    self.solver.display_progress = True
+                    self.solver.report_continuously = True
+                elif key == "time_step_size":
+                    self.solver.inith = value
+                    self._time_step_size = value
+                else:
+                    raise ValueError("Invalid paramater passed to _AssimuloIDA object.")
+            
+        def solve(self):
+            assert self._max_time_steps is not None or self._time_step_size is not None
+            if self._time_step_size is not None:
+                all_t = [0]
+                all_t_arange = self._time_step_size + arange(self._initial_time, self._final_time, self._time_step_size)
+                all_t.extend(all_t_arange.tolist())
+            elif self._max_time_steps is not None:
+                all_t = linspace(self._initial_time, self._final_time, num=self._max_time_steps+1)
+                all_t = all_t.tolist()
+            all_times, all_solutions, all_solutions_dot = self.solver.simulate(self._final_time, ncp_list=all_t)
+            # Convert all_solutions to a list of Function
+            all_solutions_as_functions = list()
+            for (t, solution) in zip(all_times, all_solutions):
+                solution_as_function = Function(self.solution.vector().N)
+                solution_as_function.vector()[:] = solution.reshape((-1, 1))
+                # Fix bcs
+                if self.bc_eval is not None:
+                    self.current_bc = DirichletBC(self.sample_jacobian, self.sample_residual, self.bc_eval(t))
+                    self.current_bc.apply_to_vector(solution_as_function.vector())
+                all_solutions_as_functions.append(solution_as_function)
+                if self._monitor is not None:
+                    self._monitor(t, solution_as_function.vector())
+            self.solution.vector()[:] = all_solutions_as_functions[-1].vector()
+            return all_solutions_as_functions
+            
