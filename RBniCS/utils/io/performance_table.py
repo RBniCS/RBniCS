edc9bab8924ca69cfb149880f68e9d1ca444f606
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-12-02 11:13:14 +0100

    Add a class method to suppress groups in error analysis table. Its typical use will be in a subclass of a reduction class, where before calling the parent error analysis method the user disables some groups that (even though computed) are not required to be printed. A class method (rather than a standard one) is required because the ErrorAnalysisTable object is defined inside the parent method. A typical use case of this is error analysis of dual problem for non compliant outputs, where only the error on the solution is meaningful.

--- RBniCS/utils/io/performance_table.py
+++ RBniCS/utils/io/performance_table.py
@@ -26,6 +26,10 @@ from numpy import zeros as Content
 from numpy import min, exp, mean, log, max
 
 class PerformanceTable(object):
+    
+    # Storage for class method
+    _suppressed_groups = list()
+    
     def __init__(self, testing_set):
         self._columns = dict() # string to Content matrix
         self._columns_operations = dict() # string to tuple
@@ -55,7 +59,15 @@ class PerformanceTable(object):
             self._columns_operations[column_name] = operations
         else:
             raise ValueError("Invalid operation in PerformanceTable")
-                
+    
+    @classmethod
+    def suppress_group(cls, group_name):
+        cls._suppressed_groups.append(group_name)
+        
+    @classmethod
+    def clear_suppressed_groups(cls):
+        cls._suppressed_groups = list()
+    
     def __getitem__(self, args):
         assert len(args) == 3
         return self._columns[args[0]][args[1] - self._Nmin, args[2]]
@@ -67,60 +79,61 @@ class PerformanceTable(object):
     def __str__(self):
         output = ""
         for group in self._group_names_sorted:
-            table_index = list() # of strings
-            table_header = dict() # from string to string
-            table_content = dict() # from string to Content array
-            column_size = dict() # from string to int
-            # First column should be the reduced space dimension
-            table_index.append("N")
-            table_header["N"] = "N"
-            table_content["N"] = range(self._Nmin, self._Nmax + 1)
-            column_size["N"] = max([max([len(str(x)) for x in table_content["N"]]), len("N")])
-            # Then fill in with postprocessed data
-            for column in self._groups[group]:
-                for operation in self._columns_operations[column]:
-                    # Set header
-                    if operation in ("min", "max"):
-                        current_table_header = operation + "(" + column + ")"
-                        current_table_index = operation + "_" + column
-                    elif operation == "mean":
-                        current_table_header = "gmean(" + column + ")"
-                        current_table_index = "gmean_" + column
-                    else:
-                        raise ValueError("Invalid operation in PerformanceTable")
-                    table_index.append(current_table_index)
-                    table_header[current_table_index] = current_table_header
-                    # Compute the required operation of each column over the second index (testing set)
-                    table_content[current_table_index] = Content((self._Nmax - self._Nmin + 1,))
-                    for n in range(self._Nmin, self._Nmax + 1):
-                        if operation == "min":
-                            current_table_content = min(self._columns[column][n - self._Nmin, :])
+            if not group in self._suppressed_groups:
+                table_index = list() # of strings
+                table_header = dict() # from string to string
+                table_content = dict() # from string to Content array
+                column_size = dict() # from string to int
+                # First column should be the reduced space dimension
+                table_index.append("N")
+                table_header["N"] = "N"
+                table_content["N"] = range(self._Nmin, self._Nmax + 1)
+                column_size["N"] = max([max([len(str(x)) for x in table_content["N"]]), len("N")])
+                # Then fill in with postprocessed data
+                for column in self._groups[group]:
+                    for operation in self._columns_operations[column]:
+                        # Set header
+                        if operation in ("min", "max"):
+                            current_table_header = operation + "(" + column + ")"
+                            current_table_index = operation + "_" + column
                         elif operation == "mean":
-                            current_table_content = exp(mean(log(self._columns[column][n - self._Nmin, :])))
-                        elif operation == "max":
-                            current_table_content = max(self._columns[column][n - self._Nmin, :])
+                            current_table_header = "gmean(" + column + ")"
+                            current_table_index = "gmean_" + column
                         else:
                             raise ValueError("Invalid operation in PerformanceTable")
-                        table_content[current_table_index][n - self._Nmin] = current_table_content
-                    # Get the width of the columns
-                    column_size[current_table_index] = max([max([len(str(x)) for x in table_content[current_table_index]]), len(current_table_header)])
-            # Prepare formetter for string conversion
-            formatter = ""
-            for (column_index, column_name) in enumerate(table_index):
-                formatter += "{" + str(column_index) + ":<{" + column_name + "}}"
-                if column_index < len(table_index) - 1:
-                    formatter += "\t"
-            # Print the header
-            current_line = list()
-            for t in table_index:
-                current_line.append(table_header[t])
-            output += formatter.format(*current_line, **column_size) + "\n"
-            # Print the content
-            for n in range(self._Nmin, self._Nmax + 1):
+                        table_index.append(current_table_index)
+                        table_header[current_table_index] = current_table_header
+                        # Compute the required operation of each column over the second index (testing set)
+                        table_content[current_table_index] = Content((self._Nmax - self._Nmin + 1,))
+                        for n in range(self._Nmin, self._Nmax + 1):
+                            if operation == "min":
+                                current_table_content = min(self._columns[column][n - self._Nmin, :])
+                            elif operation == "mean":
+                                current_table_content = exp(mean(log(self._columns[column][n - self._Nmin, :])))
+                            elif operation == "max":
+                                current_table_content = max(self._columns[column][n - self._Nmin, :])
+                            else:
+                                raise ValueError("Invalid operation in PerformanceTable")
+                            table_content[current_table_index][n - self._Nmin] = current_table_content
+                        # Get the width of the columns
+                        column_size[current_table_index] = max([max([len(str(x)) for x in table_content[current_table_index]]), len(current_table_header)])
+                # Prepare formetter for string conversion
+                formatter = ""
+                for (column_index, column_name) in enumerate(table_index):
+                    formatter += "{" + str(column_index) + ":<{" + column_name + "}}"
+                    if column_index < len(table_index) - 1:
+                        formatter += "\t"
+                # Print the header
                 current_line = list()
                 for t in table_index:
-                    current_line.append(table_content[t][n - self._Nmin])
+                    current_line.append(table_header[t])
                 output += formatter.format(*current_line, **column_size) + "\n"
-            output += "\n"
+                # Print the content
+                for n in range(self._Nmin, self._Nmax + 1):
+                    current_line = list()
+                    for t in table_index:
+                        current_line.append(table_content[t][n - self._Nmin])
+                    output += formatter.format(*current_line, **column_size) + "\n"
+                output += "\n"
         return output[:-2] # remove the last two newlines
         
