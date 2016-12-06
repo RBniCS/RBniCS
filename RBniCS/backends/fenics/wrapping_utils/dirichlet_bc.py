9794f9cd027c342b11588bf2ac09cdab800d55c2
Francesco Ballarin <francesco.ballarin@sissa.it>
2016-12-06 08:42:02 +0100

    Extend FEniCS DirichletBC to handle multiplication by a scalar

--- RBniCS/backends/fenics/wrapping_utils/dirichlet_bc.py
+++ RBniCS/backends/fenics/wrapping_utils/dirichlet_bc.py
@@ -22,13 +22,27 @@
 #  @author Gianluigi Rozza    <gianluigi.rozza@sissa.it>
 #  @author Alberto   Sartori  <alberto.sartori@sissa.it>
 
-import dolfin
+from dolfin import Constant, DirichletBC as dolfin_DirichletBC
 
 def DirichletBC(V, g, subdomains, subdomain_id):
-    output = dolfin.DirichletBC(V, g, subdomains, subdomain_id)
+    output = dolfin_DirichletBC(V, g, subdomains, subdomain_id)
     output.subdomains = subdomains # this is currently not available in the public interface
     output.subdomain_id = subdomain_id # this is currently not available in the public interface
     output.value = g # this is available but it is cast to a base type, and it makes performing the sum not possible
     output.function_space = V # this is already available as a method, replace it with an attribute for consistency
     return output
+
+# Add a multiplication operator by a scalar
+def mul_by_scalar(self, other):
+    if isinstance(other, (float, int)):
+        V = self.function_space
+        g = Constant(other)*self.value
+        subdomains = self.subdomains
+        subdomain_id = self.subdomain_id
+        return DirichletBC(V, g, subdomains, subdomain_id)
+    else:
+        return NotImplemented
+        
+setattr(dolfin_DirichletBC, "__mul__", mul_by_scalar)
+setattr(dolfin_DirichletBC, "__rmul__", mul_by_scalar)
 
