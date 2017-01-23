a7ce7766a13f874cf9d5a5fa614ea22aec7bdee6
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-01-23 11:31:19 +0100

    Add second example to tutorial 18

---
+++ tutorials/18_elliptic_optimal_control/data/generate_mesh.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2015-2017 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from mshr import *
+
+def generate_mesh1():
+    pass # Uses the same mesh as tutorial 04
+    
+def generate_mesh2():
+    # Create mesh
+    rectangle = Rectangle(Point(0., 0.), Point(2.5, 1.))
+    domain = rectangle
+    subdomain = dict()
+    subdomain[1] = Rectangle(Point(0.2, 0.2), Point(0.8, 0.8))
+    subdomain[2] = Rectangle(Point(1.2, 0.2), Point(2.5, 0.8))
+    domain = rectangle
+    for i, s in subdomain.iteritems():
+        domain.set_subdomain(i, subdomain[i])
+    mesh = generate_mesh(domain, 64)
+    plot(mesh)
+    interactive()
+
+    # Create subdomains
+    subdomains = MeshFunction("size_t", mesh, 2, mesh.domains())
+    plot(subdomains)
+    interactive()
+
+    # Create boundaries
+    class Left(SubDomain):
+        def __init__(self):
+            SubDomain.__init__(self)
+        def inside(self, x, on_boundary):
+            return on_boundary and abs(x[0] - 0.) < DOLFIN_EPS 
+       
+    class Right(SubDomain):
+        def __init__(self):
+            SubDomain.__init__(self)
+        def inside(self, x, on_boundary):
+            return on_boundary and abs(x[0] - 2.5) < DOLFIN_EPS 
+            
+    class Bottom(SubDomain):
+        def __init__(self, x_min, x_max):
+            SubDomain.__init__(self)
+            self.x_min = x_min
+            self.x_max = x_max
+        def inside(self, x, on_boundary):
+            return on_boundary and abs(x[1] - 0.) < DOLFIN_EPS and x[0] >= self.x_min and x[0] <= self.x_max
+            
+    class Top(SubDomain):
+        def __init__(self, x_min, x_max):
+            SubDomain.__init__(self)
+            self.x_min = x_min
+            self.x_max = x_max
+        def inside(self, x, on_boundary):
+            return on_boundary and abs(x[1] - 1.) < DOLFIN_EPS and x[0] >= self.x_min and x[0] <= self.x_max
+            
+    boundaries = FacetFunction("size_t", mesh)
+    boundaries.set_all(0)
+    left = Left()
+    left.mark(boundaries, 1)
+    bottom1 = Bottom(0., 1.) 
+    bottom1.mark(boundaries, 1)
+    top1 = Top(0., 1.) 
+    top1.mark(boundaries, 1)
+    bottom2 = Bottom(1., 2.5) 
+    bottom2.mark(boundaries, 2)
+    top2 = Top(1., 2.5)
+    top2.mark(boundaries, 2)
+    right = Right()
+    right.mark(boundaries, 3)
+    plot(boundaries)
+    interactive()
+
+    # Save
+    File("mesh2.xml") << mesh
+    File("mesh2_physical_region.xml") << subdomains
+    File("mesh2_facet_region.xml") << boundaries
+    
+generate_mesh1()
+generate_mesh2()
---
+++ tutorials/18_elliptic_optimal_control/solve_elliptic_optimal_control_2.py
@@ -0,0 +1,207 @@
+# Copyright (C) 2015-2017 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+## @file solve_graetz.py
+#  @brief Example 4: Graetz test case
+#
+#  @author Francesco Ballarin <francesco.ballarin@sissa.it>
+#  @author Gianluigi Rozza    <gianluigi.rozza@sissa.it>
+#  @author Alberto   Sartori  <alberto.sartori@sissa.it>
+
+from dolfin import *
+from RBniCS import *
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~     EXAMPLE 18: OPTIMAL CONTROL CLASS     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
+class EllipticOptimalControl(EllipticOptimalControlProblem):
+    
+    ###########################     CONSTRUCTORS     ########################### 
+    ## @defgroup Constructors Methods related to the construction of the reduced order model object
+    #  @{
+    
+    ## Default initialization of members
+    def __init__(self, V, **kwargs):
+        # Call the standard initialization
+        EllipticOptimalControlProblem.__init__(self, V, **kwargs)
+        # ... and also store FEniCS data structures for assembly
+        assert "subdomains" in kwargs
+        assert "boundaries" in kwargs
+        self.subdomains, self.boundaries = kwargs["subdomains"], kwargs["boundaries"]
+        yup = TrialFunction(V)
+        (self.y, self.u, self.p) = split(yup)
+        qvz = TestFunction(V)
+        (self.q, self.v, self.z) = split(qvz)
+        self.dx = Measure("dx")(subdomain_data=subdomains)
+        self.ds = Measure("ds")(subdomain_data=boundaries)
+        # Regularization coefficient
+        self.alpha = 0.01
+        # Store the velocity expression
+        self.vel = Expression("x[1]*(1-x[1])", element=self.V.sub(0).ufl_element())
+                
+    #  @}
+    ########################### end - CONSTRUCTORS - end ########################### 
+        
+    ###########################     PROBLEM SPECIFIC     ########################### 
+    ## @defgroup ProblemSpecific Problem specific methods
+    #  @{
+    
+    ## Return theta multiplicative terms of the affine expansion of the problem.
+    def compute_theta(self, term):
+        mu1 = self.mu[0]
+        mu2 = self.mu[1]
+        mu3 = self.mu[2]
+        if term == "a" or term == "at":
+            theta_a0 = 1.0/mu1
+            theta_a1 = 1.0
+            return (theta_a0, theta_a1)
+        elif term == "c" or term == "ct":
+            theta_c0 = 1.0
+            return (theta_c0,)
+        elif term == "m":
+            theta_m0 = 1.0
+            return (theta_m0,)
+        elif term == "n":
+            theta_n0 = self.alpha
+            return (theta_n0,)
+        elif term == "f":
+            theta_f0 = 1.0
+            return (theta_f0,)
+        elif term == "g":
+            theta_g0 = mu2
+            theta_g1 = mu3
+            return (theta_g0, theta_g1)
+        elif term == "h":
+            theta_h0 = 0.64*mu2**2 + 0.78*mu3**2
+            return (theta_h0,)
+        elif term == "dirichlet_bc_y":
+            theta_bc0 = 1.
+            return (theta_bc0,)
+        else:
+            raise ValueError("Invalid term for compute_theta().")
+                    
+    ## Return forms resulting from the discretization of the affine expansion of the problem operators.
+    def assemble_operator(self, term):
+        dx = self.dx
+        if term == "a":
+            y = self.y
+            q = self.q
+            vel = self.vel
+            a0 = inner(grad(y),grad(q))*dx
+            a1 = vel*y.dx(0)*q*dx
+            return (a0, a1)
+        elif term == "at":
+            z = self.z
+            p = self.p
+            vel = self.vel
+            at0 = inner(grad(z),grad(p))*dx
+            at1 = - vel*z.dx(0)*p*dx
+            return (at0, at1)
+        elif term == "c":
+            u = self.u
+            q = self.q
+            c0 = u*q*dx
+            return (c0,)
+        elif term == "ct":
+            v = self.v
+            p = self.p
+            ct0 = v*p*dx
+            return (ct0,)
+        elif term == "m":
+            y = self.y
+            z = self.z
+            m0 = y*z*dx(1) + y*z*dx(2)
+            return (m0,)
+        elif term == "n":
+            u = self.u
+            v = self.v
+            n0 = u*v*dx
+            return (n0,)
+        elif term == "f":
+            q = self.q
+            f0 = Constant(0.0)*q*dx
+            return (f0,)
+        elif term == "g":
+            z = self.z
+            g0 = z*dx(1)
+            g1 = z*dx(2)
+            return (g0, g1)
+        elif term == "h":
+            h0 = 1.0
+            return (h0,)
+        elif term == "dirichlet_bc_y":
+            bc0 = [DirichletBC(self.V.sub(0), Constant(i), self.boundaries, i) for i in (1, 2)]
+            return (bc0,)
+        elif term == "dirichlet_bc_p":
+            bc0 = [DirichletBC(self.V.sub(2), Constant(0.0), self.boundaries, i) for i in (1, 2)]
+            return (bc0,)
+        elif term == "inner_product_y":
+            y = self.y
+            q = self.q
+            x0 = inner(grad(y),grad(q))*dx
+            return (x0,)
+        elif term == "inner_product_u":
+            u = self.u
+            v = self.v
+            x0 = u*v*dx
+            return (x0,)
+        elif term == "inner_product_p":
+            z = self.z
+            p = self.p
+            x0 = inner(grad(z),grad(p))*dx
+            return (x0,)
+        else:
+            raise ValueError("Invalid term for assemble_operator().")
+        
+    #  @}
+    ########################### end - PROBLEM SPECIFIC - end ########################### 
+    
+#~~~~~~~~~~~~~~~~~~~~~~~~~     EXAMPLE 18: MAIN PROGRAM     ~~~~~~~~~~~~~~~~~~~~~~~~~# 
+
+# 1. Read the mesh for this problem
+mesh = Mesh("data/mesh2.xml")
+subdomains = MeshFunction("size_t", mesh, "data/mesh2_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "data/mesh2_facet_region.xml")
+
+# 2. Create Finite Element space (Lagrange P1)
+scalar_element = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+element = MixedElement(scalar_element, scalar_element, scalar_element)
+V = FunctionSpace(mesh, element, components=["y", "u", "p"])
+
+# 3. Allocate an object of the EllipticOptimalControl class
+elliptic_optimal_control = EllipticOptimalControl(V, subdomains=subdomains, boundaries=boundaries)
+mu_range = [(3.0, 20.0), (0.5, 1.5), (1.5, 2.5)]
+elliptic_optimal_control.set_mu_range(mu_range)
+
+# 4. Prepare reduction with a reduced basis method
+pod_galerkin_method = PODGalerkin(elliptic_optimal_control)
+pod_galerkin_method.set_Nmax(20)
+
+# 5. Perform the offline phase
+first_mu = (3.0, 1.0, 2.0)
+elliptic_optimal_control.set_mu(first_mu)
+pod_galerkin_method.initialize_training_set(100)
+reduced_elliptic_optimal_control = pod_galerkin_method.offline()
+
+# 6. Perform an online solve
+online_mu = (10.0, 0.6, 2.8)
+reduced_elliptic_optimal_control.set_mu(online_mu)
+reduced_elliptic_optimal_control.solve()
+reduced_elliptic_optimal_control.export_solution("EllipticOptimalControl", "online_solution")
+
+# 7. Perform an error analysis
+pod_galerkin_method.initialize_testing_set(100)
+pod_galerkin_method.error_analysis()
+
