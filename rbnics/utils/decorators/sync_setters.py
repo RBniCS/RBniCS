2df808a719653ed80e7f8949a903fc503423e19d
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-06-16 13:12:37 +0200

    Reimplement sync_setters decorators to avoid recursion

--- rbnics/utils/decorators/sync_setters.py
+++ rbnics/utils/decorators/sync_setters.py
@@ -17,7 +17,6 @@
 #
 
 import types
-from rbnics.utils.decorators.override import override
 
 def sync_setters__internal(other_object__name, method__name, private_attribute__name, method__decorator=None):
     def sync_setters_decorator(__init__):
@@ -29,34 +28,64 @@ def sync_setters__internal(other_object__name, method__name, private_attribute__
             other_object = getattr(self, other_object__name)
             # Sync setters only if the other object is not None
             if other_object is not None:
-                # Get original methods
-                self__original_method = getattr(self, method__name)
-                other_object__original_method = getattr(other_object, method__name)
-                # Override my setter to propagate from self to other_object
-                def self__overridden_method(self, arg):
-                    self__original_method(arg)
-                    self_attribute = getattr(self, private_attribute__name)
-                    other_attribute = getattr(other_object, private_attribute__name)
-                    if other_attribute is not self_attribute:
-                        other_object__original_method(self_attribute)
+                # Initialize private storage
+                if method__name not in _synced_setters:
+                    _synced_setters[method__name] = dict()
+                # Detect if either self or other_object are already in sync with somebody else
+                all_synced_setters_for_method_self = None
+                if self in _synced_setters[method__name]:
+                    all_synced_setters_for_method_self = _synced_setters[method__name][self]
+                all_synced_setters_for_method_other_object = None
+                if other_object in _synced_setters[method__name]:
+                    all_synced_setters_for_method_other_object = _synced_setters[method__name][other_object]
+                # Add current methods to the set of syncronized setters in storage
+                if (
+                    all_synced_setters_for_method_self is not None
+                        and
+                    all_synced_setters_for_method_other_object is not None
+                ):
+                    assert all_synced_setters_for_method_self is all_synced_setters_for_method_other_object
+                elif (
+                    all_synced_setters_for_method_self is None
+                        and
+                    all_synced_setters_for_method_other_object is not None
+                ):
+                    all_synced_setters_for_method = all_synced_setters_for_method_other_object
+                    all_synced_setters_for_method.add((self, getattr(self, method__name)))
+                    _synced_setters[method__name][self] = all_synced_setters_for_method
+                elif (
+                    all_synced_setters_for_method_self is not None
+                        and
+                    all_synced_setters_for_method_other_object is None
+                ):
+                    all_synced_setters_for_method = all_synced_setters_for_method_self
+                    all_synced_setters_for_method.add((other_object, getattr(other_object, method__name)))
+                    _synced_setters[method__name][other_object] = all_synced_setters_for_method
+                else:
+                    all_synced_setters_for_method = set()
+                    all_synced_setters_for_method.add((self, getattr(self, method__name)))
+                    all_synced_setters_for_method.add((other_object, getattr(other_object, method__name)))
+                    _synced_setters[method__name][self] = all_synced_setters_for_method
+                    _synced_setters[method__name][other_object] = all_synced_setters_for_method
+                # Now both storage and local variable should be consistent between self and other_object, 
+                # and pointing to the same memory location
+                assert _synced_setters[method__name][self] is _synced_setters[method__name][other_object]
+                # Override both self and other_object setters to propagate to all synced setters
+                def overridden_method(self_, arg):
+                    if method__name not in _synced_setters__disabled_methods:
+                        all_synced_setters = _synced_setters[method__name][self_]
+                        for (obj, setter) in all_synced_setters:
+                            if getattr(obj, private_attribute__name) is not arg:
+                                setter(arg)
                 if method__decorator is not None:
-                    self__overridden_method = method__decorator(self__overridden_method)
-                self__overridden_method = override(self__overridden_method)
-                setattr(self, method__name, types.MethodType(self__overridden_method, self))
-                # Override setter of other_object to propagate from other_object to self
-                def other_object__overridden_method(_, arg): # _ is other_object, self is an instance of the parent class
-                    other_object__original_method(arg)
-                    other_attribute = getattr(other_object, private_attribute__name)
-                    self_attribute = getattr(self, private_attribute__name)
-                    if self_attribute is not other_attribute:
-                        self__original_method(other_attribute)
-                if method__decorator is not None:
-                    other_object__overridden_method = method__decorator(other_object__overridden_method)
-                other_object__overridden_method = override(other_object__overridden_method)
-                setattr(other_object, method__name, types.MethodType(other_object__overridden_method, other_object))
-                # Make sure that the value of my attribute is in sync with the value the is currently 
+                    overridden_method = method__decorator(overridden_method)
+                if all_synced_setters_for_method_self is None:
+                    setattr(self, method__name, types.MethodType(overridden_method, self))
+                if all_synced_setters_for_method_other_object is None:
+                    setattr(other_object, method__name, types.MethodType(overridden_method, other_object))
+                # Make sure that the value of my attribute is in sync with the value that is currently 
                 # stored in other_object, because it was set before overriding was carried out
-                self__original_method(getattr(other_object, private_attribute__name))
+                getattr(self, method__name)(getattr(other_object, private_attribute__name))
         
         return __synced__init__
     return sync_setters_decorator
@@ -68,26 +97,23 @@ def sync_setters(other_object__name, method__name, private_attribute__name):
     elif method__name == "set_mu_range":
         def set_mu_range__decorator(set_mu_range__method):
             def set_mu_range__decorated(self_, mu_range):
-                try:
-                    set_mu_range__method(self_, mu_range)
-                except AssertionError as assertion:
-                    if str(assertion) == "mu and mu_range must have the same length":
-                        # This may happen because mu_range has not been set yet when
-                        # called recursively across e.g. SCM, EIM, etc.
-                        # Temporarily disable it, since it will be called anyway at 
-                        # the last recursion step
-                        original_set_mu = self_.set_mu
-                        def disabled_set_mu(self_, mu):
-                            pass
-                        setattr(self_, "set_mu", types.MethodType(disabled_set_mu, self_))
-                        # Call again set_mu_range
-                        set_mu_range__method(self_, mu_range)
-                        # Restore original set_mu
-                        setattr(self_, "set_mu", original_set_mu)
-                    else:
-                        raise
+                # set_mu_range by defaults calls set_mu. Since set_mu
+                # (1) requires a properly initialized mu range, but
+                # (2) it has been overridden to be kept in sync, also
+                #     for object which have not been initialized yet, 
+                # we first disable set_mu
+                _synced_setters__disabled_methods.add("set_mu")
+                # We set (and sync) the mu range
+                set_mu_range__method(self_, mu_range)
+                # Finally, we restore the original set_mu and set (and sync)
+                # the value of mu so that it has the correct length,
+                # as done in ParametrizedProblem
+                _synced_setters__disabled_methods.remove("set_mu")
+                self_.set_mu(tuple([r[0] for r in mu_range]))
             return set_mu_range__decorated
         return sync_setters__internal(other_object__name, method__name, private_attribute__name, set_mu_range__decorator)
     else:
         raise AssertionError("Invalid method in sync_setters.")
     
+_synced_setters = dict()
+_synced_setters__disabled_methods = set()
