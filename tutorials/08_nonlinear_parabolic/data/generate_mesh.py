5b02694c03c921f79fd8aa2f75e7f5e1d7242b1c
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-07-29 16:45:26 +0200

    Add first draft of tutorial 8 on nonlinear parabolic problems

---
+++ tutorials/08_nonlinear_parabolic/data/generate_mesh.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2015-2017 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+
+# Create mesh
+mesh = UnitIntervalMesh(1024)
+
+# Create subdomains
+subdomains = CellFunction("size_t", mesh)
+subdomains.set_all(0)
+
+# Create boundaries
+class Left(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary and abs(x[0]) < DOLFIN_EPS
+
+class Right(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary and abs(x[0] - 1.) < DOLFIN_EPS
+        
+boundaries = FacetFunction("size_t", mesh)
+boundaries.set_all(0)
+left = Left()
+left.mark(boundaries, 1)
+right = Right()
+right.mark(boundaries, 2)
+
+# Save
+File("interval.xml") << mesh
+File("interval_physical_region.xml") << subdomains
+File("interval_facet_region.xml") << boundaries
+XDMFFile("interval.xdmf").write(mesh)
+XDMFFile("interval_physical_region.xdmf").write(subdomains)
+XDMFFile("interval_facet_region.xdmf").write(boundaries)
---
+++ tutorials/08_nonlinear_parabolic/utils/plot_phase_space.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2015-2017 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import matplotlib.pyplot as plt
+
+def plot_phase_space(solution_over_time, reduced_solution_over_time, Z, x, folder, filename):
+    plt.figure()
+    (all_u1, all_u2) = (list(), list())
+    (all_reduced_u1, all_reduced_u2) = (list(), list())
+    assert len(solution_over_time) == len(reduced_solution_over_time)
+    for (solution, reduced_solution) in zip(solution_over_time, reduced_solution_over_time):
+        (u1, u2) = solution.split()
+        all_u1.append(u1(x))
+        all_u2.append(u2(x))
+        (reduced_u1, reduced_u2) = (Z*reduced_solution).split()
+        all_reduced_u1.append(reduced_u1(x))
+        all_reduced_u2.append(reduced_u2(x))
+    offline_line, = plt.plot(all_u1, all_u2, label="Offline solution")
+    online_line, = plt.plot(all_reduced_u1, all_reduced_u2, label="Online solution")
+    plt.legend(handles=[offline_line, online_line])
+    plt.savefig(folder + "/" + filename + ".png")
