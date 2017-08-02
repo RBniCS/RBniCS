fd1a306e1ff20485f1d84bec0195b9d27cea8190
Francesco Ballarin <francesco.ballarin@sissa.it>
2017-08-02 13:21:29 +0200

    Output to *.pvd (and *.xdmf) is only supported for scalar fields, 2 or 3 dimensional vectors or 4 or 9 dimensional tensors. Split all other cases. Thanks to Federico Pichi <fpichi@sissa.it> for the test case that highlighted this issue.

--- rbnics/backends/dolfin/wrapping/function_load.py
+++ rbnics/backends/dolfin/wrapping/function_load.py
@@ -33,7 +33,7 @@ def function_load(fun, directory, filename, suffix=None):
             for (component, index) in fun_V._component_to_index.iteritems():
                 sub_fun_V = get_function_subspace(fun_V, component)
                 sub_fun = Function(sub_fun_V)
-                if not _read_from_xdmf_file(sub_fun, directory, filename + "_component_" + str(index), suffix, component):
+                if not _read_from_xdmf_file(sub_fun, directory, filename, suffix, component, component):
                     return False
                 else:
                     extended_sub_fun = function_extend_or_restrict(sub_fun, None, fun_V, component, weight=None, copy=True)
@@ -41,7 +41,7 @@ def function_load(fun, directory, filename, suffix=None):
                     fun.vector().apply("add")
             return True
         else:
-            return _read_from_xdmf_file(fun, directory, filename, suffix, "function")
+            return _read_from_xdmf_file(fun, directory, filename, suffix)
     
 def _read_from_xml_file(fun, directory, filename, suffix):
     if suffix is not None:
@@ -56,27 +56,54 @@ def _read_from_xml_file(fun, directory, filename, suffix):
         file_ >> fun
     return file_exists
     
-def _read_from_xdmf_file(fun, directory, filename, suffix, component):
-    full_filename_checkpoint = str(directory) + "/" + filename + "_checkpoint.xdmf"
-    file_exists = False
-    if is_io_process() and os.path.exists(full_filename_checkpoint):
-        file_exists = True
-    file_exists = is_io_process.mpi_comm.bcast(file_exists, root=is_io_process.root)
-    if file_exists:
-        if suffix is not None:
-            if full_filename_checkpoint in _all_xdmf_files:
-                assert _all_xdmf_latest_suffix[full_filename_checkpoint] == suffix - 1
-                _all_xdmf_latest_suffix[full_filename_checkpoint] = suffix
+def _read_from_xdmf_file(fun, directory, filename, suffix, component=None, function_name=None):
+    if component is not None:
+        filename = filename + "_component_" + component
+    if function_name is None:
+        function_name = "function"
+    fun_rank = fun.value_rank()
+    fun_dim = fun.value_size()
+    assert fun_rank <= 2
+    if (
+        (fun_rank is 1 and fun_dim not in (2, 3))
+            or
+        (fun_rank is 2 and fun_dim not in (4, 9))
+    ):
+        fun_V = fun.function_space()
+        for i in range(fun_dim):
+            if component is not None:
+                filename_i = filename + "_subcomponent_" + str(i)
             else:
-                assert suffix == 0
-                _all_xdmf_files[full_filename_checkpoint] = XDMFFile(full_filename_checkpoint)
-                _all_xdmf_latest_suffix[full_filename_checkpoint] = 0
-            _all_xdmf_files[full_filename_checkpoint].read_checkpoint(fun, component, suffix)
-        else:
-            file_checkpoint = XDMFFile(full_filename_checkpoint)
-            file_checkpoint.read_checkpoint(fun, component, 0)
-            file_checkpoint.close()
-    return file_exists
+                filename_i = filename + "_component_" + str(i)
+            fun_i_V = get_function_subspace(fun_V, i)
+            fun_i = Function(fun_i_V)
+            if not _read_from_xdmf_file(fun_i, directory, filename, suffix, None, function_name):
+                return False
+            else:
+                extended_sub_fun = function_extend_or_restrict(fun_i, None, fun_V, component, weight=None, copy=True)
+                fun.vector().add_local(extended_sub_fun.vector().array())
+                fun.vector().apply("add")
+    else:
+        full_filename_checkpoint = str(directory) + "/" + filename + "_checkpoint.xdmf"
+        file_exists = False
+        if is_io_process() and os.path.exists(full_filename_checkpoint):
+            file_exists = True
+        file_exists = is_io_process.mpi_comm.bcast(file_exists, root=is_io_process.root)
+        if file_exists:
+            if suffix is not None:
+                if full_filename_checkpoint in _all_xdmf_files:
+                    assert _all_xdmf_latest_suffix[full_filename_checkpoint] == suffix - 1
+                    _all_xdmf_latest_suffix[full_filename_checkpoint] = suffix
+                else:
+                    assert suffix == 0
+                    _all_xdmf_files[full_filename_checkpoint] = XDMFFile(full_filename_checkpoint)
+                    _all_xdmf_latest_suffix[full_filename_checkpoint] = 0
+                _all_xdmf_files[full_filename_checkpoint].read_checkpoint(fun, function_name, suffix)
+            else:
+                file_checkpoint = XDMFFile(full_filename_checkpoint)
+                file_checkpoint.read_checkpoint(fun, function_name, 0)
+                file_checkpoint.close()
+        return file_exists
     
 _all_xdmf_files = dict()
 _all_xdmf_latest_suffix = dict()
--- rbnics/backends/dolfin/wrapping/function_save.py
+++ rbnics/backends/dolfin/wrapping/function_save.py
@@ -31,7 +31,7 @@ def function_save(fun, directory, filename, suffix=None):
         if hasattr(fun_V, "_component_to_index") and len(fun_V._component_to_index) > 1:
             for (component, index) in fun_V._component_to_index.iteritems():
                 sub_fun = function_extend_or_restrict(fun, component, get_function_subspace(fun_V, component), None, weight=None, copy=True)
-                _write_to_pvd_file(sub_fun, directory, filename + "_component_" + str(index), suffix)
+                _write_to_pvd_file(sub_fun, directory, filename, suffix, component)
         else:
             _write_to_pvd_file(fun, directory, filename, suffix)
         _write_to_xml_file(fun, directory, filename, suffix)
@@ -39,9 +39,9 @@ def function_save(fun, directory, filename, suffix=None):
         if hasattr(fun_V, "_component_to_index") and len(fun_V._component_to_index) > 1:
             for (component, index) in fun_V._component_to_index.iteritems():
                 sub_fun = function_extend_or_restrict(fun, component, get_function_subspace(fun_V, component), None, weight=None, copy=True)
-                _write_to_xdmf_file(sub_fun, directory, filename + "_component_" + str(index), suffix, component)
+                _write_to_xdmf_file(sub_fun, directory, filename, suffix, component, component)
         else:
-            _write_to_xdmf_file(fun, directory, filename, suffix, "function")
+            _write_to_xdmf_file(fun, directory, filename, suffix)
     
 def _write_to_xml_file(fun, directory, filename, suffix):
     if suffix is not None:
@@ -50,60 +50,98 @@ def _write_to_xml_file(fun, directory, filename, suffix):
     file_ = File(full_filename)
     file_ << fun
     
-def _write_to_pvd_file(fun, directory, filename, suffix):
-    full_filename = str(directory) + "/" + filename + ".pvd"
-    if suffix is not None:
-        if full_filename in _all_pvd_files:
-            assert _all_pvd_latest_suffix[full_filename] == suffix - 1
-            _all_pvd_latest_suffix[full_filename] = suffix
-        else:
-            assert suffix == 0
-            _all_pvd_files[full_filename] = File(full_filename, "compressed")
-            _all_pvd_latest_suffix[full_filename] = 0
-            _all_pvd_functions[full_filename] = fun.copy(deepcopy=True)
-        # Make sure to always use the same function, otherwise dolfin
-        # changes the numbering and visualization is difficult in ParaView
-        assign(_all_pvd_functions[full_filename], fun)
-        _all_pvd_files[full_filename] << _all_pvd_functions[full_filename]
+def _write_to_pvd_file(fun, directory, filename, suffix, component=None):
+    if component is not None:
+        filename = filename + "_component_" + component
+    fun_rank = fun.value_rank()
+    fun_dim = fun.value_size()
+    assert fun_rank <= 2
+    if (
+        (fun_rank is 1 and fun_dim not in (2, 3))
+            or
+        (fun_rank is 2 and fun_dim not in (4, 9))
+    ):
+        funs = fun.split(deepcopy=True)
+        for (i, fun_i) in enumerate(funs):
+            if component is not None:
+                filename_i = filename + "_subcomponent_" + str(i)
+            else:
+                filename_i = filename + "_component_" + str(i)
+            _write_to_pvd_file(fun_i, directory, filename_i, suffix)
     else:
-        file_ = File(full_filename, "compressed")
-        file_ << fun
-    
-def _write_to_xdmf_file(fun, directory, filename, suffix, component):
-    full_filename_visualization = str(directory) + "/" + filename + ".xdmf"
-    full_filename_checkpoint = str(directory) + "/" + filename + "_checkpoint.xdmf"
-    if suffix is not None:
-        if full_filename_checkpoint in _all_xdmf_files:
-            assert _all_xdmf_latest_suffix[full_filename_checkpoint] == suffix - 1
-            _all_xdmf_latest_suffix[full_filename_checkpoint] = suffix
+        full_filename = str(directory) + "/" + filename + ".pvd"
+        if suffix is not None:
+            if full_filename in _all_pvd_files:
+                assert _all_pvd_latest_suffix[full_filename] == suffix - 1
+                _all_pvd_latest_suffix[full_filename] = suffix
+            else:
+                assert suffix == 0
+                _all_pvd_files[full_filename] = File(full_filename, "compressed")
+                _all_pvd_latest_suffix[full_filename] = 0
+                _all_pvd_functions[full_filename] = fun.copy(deepcopy=True)
+            # Make sure to always use the same function, otherwise dolfin
+            # changes the numbering and visualization is difficult in ParaView
+            assign(_all_pvd_functions[full_filename], fun)
+            _all_pvd_files[full_filename] << _all_pvd_functions[full_filename]
         else:
-            assert suffix == 0
+            file_ = File(full_filename, "compressed")
+            file_ << fun
+    
+def _write_to_xdmf_file(fun, directory, filename, suffix, component=None, function_name=None):
+    if component is not None:
+        filename = filename + "_component_" + component
+    if function_name is None:
+        function_name = "function"
+    fun_rank = fun.value_rank()
+    fun_dim = fun.value_size()
+    assert fun_rank <= 2
+    if (
+        (fun_rank is 1 and fun_dim not in (2, 3))
+            or
+        (fun_rank is 2 and fun_dim not in (4, 9))
+    ):
+        funs = fun.split(deepcopy=True)
+        for (i, fun_i) in enumerate(funs):
+            if component is not None:
+                filename_i = filename + "_subcomponent_" + str(i)
+            else:
+                filename_i = filename + "_component_" + str(i)
+            _write_to_xdmf_file(fun_i, directory, filename_i, suffix, None, function_name)
+    else:
+        full_filename_visualization = str(directory) + "/" + filename + ".xdmf"
+        full_filename_checkpoint = str(directory) + "/" + filename + "_checkpoint.xdmf"
+        if suffix is not None:
+            if full_filename_checkpoint in _all_xdmf_files:
+                assert _all_xdmf_latest_suffix[full_filename_checkpoint] == suffix - 1
+                _all_xdmf_latest_suffix[full_filename_checkpoint] = suffix
+            else:
+                assert suffix == 0
+                # Remove existing files if any, as new functions should not be appended,
+                # but rather overwrite existing functions
+                if is_io_process() and os.path.exists(full_filename_checkpoint):
+                    os.remove(full_filename_checkpoint)
+                    os.remove(full_filename_checkpoint.replace(".xdmf", ".h5"))
+                _all_xdmf_files[full_filename_visualization] = XDMFFile(full_filename_visualization)
+                _all_xdmf_files[full_filename_checkpoint] = XDMFFile(full_filename_checkpoint)
+                _all_xdmf_latest_suffix[full_filename_checkpoint] = 0                     # don't store these twice for both visualization
+                _all_xdmf_functions[full_filename_checkpoint] = fun.copy(deepcopy=True)   # and checkpoint, as they are the same!
+            # Make sure to always use the same function, otherwise dolfin
+            # changes the numbering and visualization is difficult in ParaView
+            assign(_all_xdmf_functions[full_filename_checkpoint], fun)
+            _all_xdmf_files[full_filename_visualization].write(_all_xdmf_functions[full_filename_checkpoint], suffix)
+            _all_xdmf_files[full_filename_checkpoint].write_checkpoint(_all_xdmf_functions[full_filename_checkpoint], function_name, suffix)
+        else:
             # Remove existing files if any, as new functions should not be appended,
             # but rather overwrite existing functions
             if is_io_process() and os.path.exists(full_filename_checkpoint):
                 os.remove(full_filename_checkpoint)
                 os.remove(full_filename_checkpoint.replace(".xdmf", ".h5"))
-            _all_xdmf_files[full_filename_visualization] = XDMFFile(full_filename_visualization)
-            _all_xdmf_files[full_filename_checkpoint] = XDMFFile(full_filename_checkpoint)
-            _all_xdmf_latest_suffix[full_filename_checkpoint] = 0                     # don't store these twice for both visualization
-            _all_xdmf_functions[full_filename_checkpoint] = fun.copy(deepcopy=True)   # and checkpoint, as they are the same!
-        # Make sure to always use the same function, otherwise dolfin
-        # changes the numbering and visualization is difficult in ParaView
-        assign(_all_xdmf_functions[full_filename_checkpoint], fun)
-        _all_xdmf_files[full_filename_visualization].write(_all_xdmf_functions[full_filename_checkpoint], suffix)
-        _all_xdmf_files[full_filename_checkpoint].write_checkpoint(_all_xdmf_functions[full_filename_checkpoint], component, suffix)
-    else:
-        # Remove existing files if any, as new functions should not be appended,
-        # but rather overwrite existing functions
-        if is_io_process() and os.path.exists(full_filename_checkpoint):
-            os.remove(full_filename_checkpoint)
-            os.remove(full_filename_checkpoint.replace(".xdmf", ".h5"))
-        file_visualization = XDMFFile(full_filename_visualization)
-        file_visualization.write(fun, 0)
-        file_visualization.close()
-        file_checkpoint = XDMFFile(full_filename_checkpoint)
-        file_checkpoint.write_checkpoint(fun, component, 0)
-        file_checkpoint.close()
+            file_visualization = XDMFFile(full_filename_visualization)
+            file_visualization.write(fun, 0)
+            file_visualization.close()
+            file_checkpoint = XDMFFile(full_filename_checkpoint)
+            file_checkpoint.write_checkpoint(fun, function_name, 0)
+            file_checkpoint.close()
         
 _all_pvd_files = dict()
 _all_pvd_latest_suffix = dict()
