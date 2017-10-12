113fd8a04a2383bc168760af711e9c81aab7d80b
Zakia Zainib <zzainib@sissa.it>
2017-10-12 12:57:12 +0200

    Save vertices mapping during mesh generation of tutorial 17

--- tutorials/17_stokes/data/generate_mesh.py
+++ tutorials/17_stokes/data/generate_mesh.py
@@ -18,24 +18,73 @@
 
 from dolfin import *
 from mshr import *
+from rbnics.backends.dolfin.wrapping import counterclockwise
+from rbnics.utils.io import PickleIO
 
 # Geometrical parameters
-t = 1.
-D = 1.
-L = 1.
-S = 1.
-H = 1.
+t = 1. # mu[0]
+D = 1. # mu[1]
+L = 1. # mu[2]
+S = 1. # mu[3]
+H = 1. # mu[4]
+theta = pi # mu[5]
+
+# Define domain
+rectangle_1 = Rectangle(Point(D, L), Point(D + H, L + t))
+rectangle_2 = Rectangle(Point(0., L + t), Point(D, L + t + S))
+rectangle_3 = Rectangle(Point(0., L), Point(D, L + t))
+rectangle_4 = Rectangle(Point(0., 0.), Point(D, L))
+domain = rectangle_1 + rectangle_2 + rectangle_3 + rectangle_4
+
+# Define vertices mappings of affine shape parametrization. These will be used
+# to partition the mesh in subdomains.
+vertices_mappings = [
+    {
+        ("1.0", "2.0"): ("mu[1]", "mu[2]+mu[0]"),
+        ("1.0", "1.0"): ("mu[1]", "mu[2]"),
+        ("2.0", "1.0"): ("mu[1]+mu[4]", "mu[2]+(mu[4]*tan(mu[5]))")
+    }, # subdomain 1
+    {
+        ("2.0", "1.0"): ("mu[1]+mu[4]", "mu[2]+(mu[4]*tan(mu[5]))"),
+        ("2.0", "2.0"): ("mu[1]+mu[4]", "mu[2]+(mu[4]*tan(mu[5]))+mu[0]"),
+        ("1.0", "2.0"): ("mu[1]", "mu[2]+mu[0]")
+    }, # subdomain 2
+    {
+        ("0.0", "3.0"): ("0.0", "mu[2]+ mu[3] +mu[0]"),
+        ("0.0", "2.0"): ("0.0", "mu[2]+mu[0]"),
+        ("1.0", "2.0"): ("mu[1]", "mu[2]+mu[0]")
+    }, # subdomain 3
+    {
+        ("1.0", "2.0"): ("mu[1]", "mu[2]+mu[0]"),
+        ("1.0", "3.0"): ("mu[1]", "mu[2]+ mu[3] +mu[0]"),
+        ("0.0", "3.0"): ("0.0", "mu[2]+ mu[3] +mu[0]")
+    }, # subdomain 4
+    {
+        ("0.0", "2.0"): ("0.0", "mu[2]+mu[0]"),
+        ("0.0", "1.0"): ("0.0", "mu[2]"),
+        ("1.0", "1.0"): ("mu[1]", "mu[2]")
+    }, # subdomain 5
+    {
+        ("1.0", "1.0"): ("mu[1]", "mu[2]"),
+        ("1.0", "2.0"): ("mu[1]", "mu[2]+mu[0]"),
+        ("0.0", "2.0"): ("0.0", "mu[2]+mu[0]")
+    }, # subdomain 6
+    {
+        ("0.0", "1.0"): ("0.0", "mu[2]"),
+        ("0.0", "0.0"): ("0.0", "0.0"),
+        ("1.0", "0.0"): ("mu[1]", "0.0")
+    }, # subdomain 7
+    {
+        ("1.0", "0.0"): ("mu[1]", "0.0"),
+        ("1.0", "1.0"): ("mu[1]", "mu[2]"),
+        ("0.0", "1.0"): ("0.0", "mu[2]")
+    } # subdomain 8
+]
 
 # Create mesh
-subdomain_1 = Rectangle(Point(D, L), Point(D + H, L + t))
-subdomain_2 = Rectangle(Point(0., L + t), Point(D, L + t + S))
-subdomain_3 = Rectangle(Point(0., L), Point(D, L + t))
-subdomain_4 = Rectangle(Point(0., 0.), Point(D, L))
-domain = subdomain_1 + subdomain_2 + subdomain_3 + subdomain_4
-domain.set_subdomain(1, subdomain_1)
-domain.set_subdomain(2, subdomain_2)
-domain.set_subdomain(3, subdomain_3)
-domain.set_subdomain(4, subdomain_4)
+for i, vertices_mapping in enumerate(vertices_mappings):
+    subdomain_i = Polygon([Point(*[float(coord) for coord in vertex]) for vertex in counterclockwise(vertices_mapping.keys())])
+    domain.set_subdomain(i + 1, subdomain_i)
 mesh = generate_mesh(domain, 50)
 
 # Create subdomains
@@ -72,6 +121,7 @@ walls_ID = 3
 walls.mark(boundaries, walls_ID)
 
 # Save to xml file
+PickleIO.save_file(vertices_mappings, ".", "t_bypass_vertices_mapping.pkl")
 File("t_bypass.xml") << mesh
 File("t_bypass_physical_region.xml") << subdomains
 File("t_bypass_facet_region.xml") << boundaries
