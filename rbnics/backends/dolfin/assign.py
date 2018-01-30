567c73d319beb9af2f26f49e032c04abb4924b1a
Francesco Ballarin <francesco.ballarin@sissa.it>
2018-01-30 13:03:05 +0100

    Try to convert UFL Operator to Function in DOLFIN assign

--- rbnics/backends/dolfin/assign.py
+++ rbnics/backends/dolfin/assign.py
@@ -16,14 +16,15 @@
 # along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
 #
 
+from ufl.core.operator import Operator
 from dolfin import assign as dolfin_assign
 from rbnics.backends.dolfin.function import Function
 from rbnics.backends.dolfin.matrix import Matrix
 from rbnics.backends.dolfin.vector import Vector
-from rbnics.backends.dolfin.wrapping import to_petsc4py
+from rbnics.backends.dolfin.wrapping import function_from_ufl_operators, to_petsc4py
 from rbnics.utils.decorators import backend_for, list_of, overload
 
-@backend_for("dolfin", inputs=((Function.Type(), list_of(Function.Type()), Matrix.Type(), Vector.Type()), (Function.Type(), list_of(Function.Type()), Matrix.Type(), Vector.Type())))
+@backend_for("dolfin", inputs=((Function.Type(), list_of(Function.Type()), Matrix.Type(), Vector.Type()), (Function.Type(), list_of(Function.Type()), Matrix.Type(), Operator, Vector.Type())))
 def assign(object_to, object_from):
     _assign(object_to, object_from)
     
@@ -32,6 +33,10 @@ def _assign(object_to: Function.Type(), object_from: Function.Type()):
     if object_from is not object_to:
         dolfin_assign(object_to, object_from)
         
+@overload
+def _assign(object_to: Function.Type(), object_from: Operator):
+    dolfin_assign(object_to, function_from_ufl_operators(object_from))
+    
 @overload
 def _assign(object_to: list_of(Function.Type()), object_from: list_of(Function.Type())):
     if object_from is not object_to:
