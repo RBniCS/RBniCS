fe701870a610c372842e4203f28d3636691240be
Francesco Ballarin <francesco.ballarin@sissa.it>
2018-03-14 17:12:20 +0100

    Missing pytest should not prevent from importing the library

--- rbnics/utils/test/tempdir.py
+++ rbnics/utils/test/tempdir.py
@@ -18,40 +18,51 @@
 
 import os
 import shutil
-import pytest
-from dolfin_utils.test import tempdir
+try:
+    import pytest
+except ImportError:
+    def tempdir(request):
+        return NotImplemented
+        
+    def save_tempdir(request):
+        return NotImplemented
+        
+    def load_tempdir(request):
+        return NotImplemented
+else:
+    from dolfin_utils.test import tempdir
 
-# Temporarily change the tempdir fixture to avoid it clearing out the temporary folder
-os_mkdir = os.mkdir
-shutil_rmtree = shutil.rmtree
+    # Temporarily change the tempdir fixture to avoid it clearing out the temporary folder
+    os_mkdir = os.mkdir
+    shutil_rmtree = shutil.rmtree
 
-def do_not_rmtree(arg):
-    pass
+    def do_not_rmtree(arg):
+        pass
 
-def mkdir_for_save(arg):
-    os_mkdir(arg.replace("_test_tensor_save", "test_tensor_io"))
-    
-def mkdir_for_load(arg):
-    pass
-    
-@pytest.fixture(scope="function")
-def save_tempdir(request):
-    function_name = request.function.__name__
-    request.function.__name__ = function_name.replace("_save", "_io")
-    os.mkdir = mkdir_for_save
-    output = tempdir(request)
-    request.function.__name__ = function_name
-    os.mkdir = os_mkdir
-    return output
+    def mkdir_for_save(arg):
+        os_mkdir(arg.replace("_test_tensor_save", "test_tensor_io"))
+        
+    def mkdir_for_load(arg):
+        pass
+        
+    @pytest.fixture(scope="function")
+    def save_tempdir(request):
+        function_name = request.function.__name__
+        request.function.__name__ = function_name.replace("_save", "_io")
+        os.mkdir = mkdir_for_save
+        output = tempdir(request)
+        request.function.__name__ = function_name
+        os.mkdir = os_mkdir
+        return output
 
-@pytest.fixture(scope="function")
-def load_tempdir(request):
-    function_name = request.function.__name__
-    request.function.__name__ = function_name.replace("_load", "_io")
-    os.mkdir = mkdir_for_load
-    shutil.rmtree = do_not_rmtree
-    output = tempdir(request)
-    request.function.__name__ = function_name
-    os.mkdir = os_mkdir
-    shutil.rmtree = shutil_rmtree
-    return output
+    @pytest.fixture(scope="function")
+    def load_tempdir(request):
+        function_name = request.function.__name__
+        request.function.__name__ = function_name.replace("_load", "_io")
+        os.mkdir = mkdir_for_load
+        shutil.rmtree = do_not_rmtree
+        output = tempdir(request)
+        request.function.__name__ = function_name
+        os.mkdir = os_mkdir
+        shutil.rmtree = shutil_rmtree
+        return output
