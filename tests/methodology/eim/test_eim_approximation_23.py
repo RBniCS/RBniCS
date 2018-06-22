f505dec1bbbb0224b8e6c26739b34146262b9b0a
Francesco Ballarin <francesco.ballarin@sissa.it>
2018-06-22 10:07:02 +0200

    Add EIM methodology test 23 on expressions/forms with a non parametrized function

---
+++ tests/methodology/eim/test_eim_approximation_23.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2015-2018 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import pytest
+from dolfin import dx, Expression, FunctionSpace, IntervalMesh, pi, project, TestFunction, TrialFunction
+from rbnics import EquispacedDistribution, ParametrizedExpression
+from rbnics.backends import ParametrizedExpressionFactory, ParametrizedTensorFactory
+from rbnics.eim.problems.eim_approximation import EIMApproximation
+from rbnics.eim.reduction_methods.eim_approximation_reduction_method import EIMApproximationReductionMethod
+from rbnics.problems.base import ParametrizedProblem
+
+@pytest.mark.parametrize("expression_type", ["Function", "Vector", "Matrix"])
+@pytest.mark.parametrize("basis_generation", ["Greedy", "POD"])
+def test_eim_approximation_23(expression_type, basis_generation):
+    """
+    This test is an extension of test 23, which splits the parameter independent part of the expression into an auxiliary
+    (non-parametrized) function. In contrast to tests 11-22, the auxiliary function is not the solution of a problem:
+    a corresponding auxiliary problem is created automatically.
+    """
+    
+    class MockProblem(ParametrizedProblem):
+        def __init__(self, V, **kwargs):
+            ParametrizedProblem.__init__(self, "")
+            self.V = V
+            
+        def name(self):
+            return "MockProblem_23_" + expression_type + "_" + basis_generation
+    
+    class ParametrizedFunctionApproximation(EIMApproximation):
+        def __init__(self, V, expression_type, basis_generation):
+            self.V = V
+            # Parametrized function to be interpolated
+            mock_problem = MockProblem(V)
+            f = project(Expression("(1-x[0])", element=V.ufl_element()), V)
+            g = ParametrizedExpression(mock_problem, "cos(3*pi*mu[0]*(1+x[0]))*exp(-mu[0]*(1+x[0]))", mu=(1., ), element=V.ufl_element())
+            #
+            folder_prefix = os.path.join("test_eim_approximation_23_tempdir", expression_type, basis_generation)
+            assert expression_type in ("Function", "Vector", "Matrix")
+            if expression_type == "Function":
+                # Call Parent constructor
+                EIMApproximation.__init__(self, mock_problem, ParametrizedExpressionFactory(f*g), folder_prefix, basis_generation)
+            elif expression_type == "Vector":
+                v = TestFunction(V)
+                form = f*g*v*dx
+                # Call Parent constructor
+                EIMApproximation.__init__(self, mock_problem, ParametrizedTensorFactory(form), folder_prefix, basis_generation)
+            elif expression_type == "Matrix":
+                u = TrialFunction(V)
+                v = TestFunction(V)
+                form = f*g*u*v*dx
+                # Call Parent constructor
+                EIMApproximation.__init__(self, mock_problem, ParametrizedTensorFactory(form), folder_prefix, basis_generation)
+            else: # impossible to arrive here anyway thanks to the assert
+                raise AssertionError("Invalid expression_type")
+
+    # 1. Create the mesh for this test
+    mesh = IntervalMesh(100, -1., 1.)
+
+    # 2. Create Finite Element space (Lagrange P1)
+    V = FunctionSpace(mesh, "Lagrange", 1)
+
+    # 3. Allocate an object of the ParametrizedFunctionApproximation class
+    parametrized_function_approximation = ParametrizedFunctionApproximation(V, expression_type, basis_generation)
+    mu_range = [(1., pi), ]
+    parametrized_function_approximation.set_mu_range(mu_range)
+
+    # 4. Prepare reduction with EIM
+    parametrized_function_reduction_method = EIMApproximationReductionMethod(parametrized_function_approximation)
+    parametrized_function_reduction_method.set_Nmax(30)
+    parametrized_function_reduction_method.set_tolerance(0.)
+
+    # 5. Perform the offline phase
+    parametrized_function_reduction_method.initialize_training_set(51, sampling=EquispacedDistribution())
+    reduced_parametrized_function_approximation = parametrized_function_reduction_method.offline()
+
+    # 6. Perform an online solve
+    online_mu = (1., )
+    reduced_parametrized_function_approximation.set_mu(online_mu)
+    reduced_parametrized_function_approximation.solve()
+
+    # 7. Perform an error analysis
+    parametrized_function_reduction_method.initialize_testing_set(100)
+    parametrized_function_reduction_method.error_analysis()
