ea9243621a6f0a41430bfdafd0d42d5e4c7d025e
Francesco Ballarin <francesco.ballarin@sissa.it>
2018-06-27 08:21:18 +0200

    Create new tests/methodology/tutorials folder. The aim of this folder is to contain variants of the tutorials which are not interesting per se for the user (e.g., do not add anything really interesting to the standard tutorials), but which may be interesting to include in the testing. This commit is adding in particular two variants of tutorial 01: the 01_thermal_block_linear variant is testing the exact evaluation of the RB error estimator with @ExactParametrizedFunctions decorator, while 01_thermal_block_nonlinear is testing @ExactParametrizedFunctions, @EIM and @DEIM decorators (and their combination) in a mock nonlinear problem (characterized by a supposedely nonlinear term which is actually linear). Results should be consistent with the standard tutorial 01.

---
+++ tests/methodology/tutorials/01_thermal_block_nonlinear/tutorial_thermal_block_nonlinear_deim_online_exact_offline.py
@@ -0,0 +1,133 @@
+# Copyright (C) 2015-2018 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from dolfin import *
+from rbnics import *
+
+@DEIM("online", basis_generation="Greedy")
+@ExactParametrizedFunctions("offline")
+class ThermalBlock(NonlinearEllipticProblem):
+    
+    # Default initialization of members
+    def __init__(self, V, **kwargs):
+        # Call the standard initialization
+        NonlinearEllipticProblem.__init__(self, V, **kwargs)
+        # ... and also store FEniCS data structures for assembly
+        assert "subdomains" in kwargs
+        assert "boundaries" in kwargs
+        self.subdomains, self.boundaries = kwargs["subdomains"], kwargs["boundaries"]
+        self.u = TrialFunction(V)
+        self.v = TestFunction(V)
+        self.dx = Measure("dx")(subdomain_data=self.subdomains)
+        self.ds = Measure("ds")(subdomain_data=self.boundaries)
+        self._nonlinear_solver_parameters.update({
+            "linear_solver": "mumps",
+            "maximum_iterations": 20,
+            "report": True
+        })
+        
+    # Return custom problem name
+    def name(self):
+        return "ThermalBlockNonlinearDEIMOnlineExactOffline"
+    
+    # Return theta multiplicative terms of the affine expansion of the problem.
+    def compute_theta(self, term):
+        mu = self.mu
+        if term in ("a", "c", "dc"):
+            theta_a0 = 0.5*mu[0]
+            theta_a1 = 0.5
+            return (theta_a0, theta_a1)
+        elif term == "f":
+            theta_f0 = mu[1]
+            return (theta_f0,)
+        else:
+            raise ValueError("Invalid term for compute_theta().")
+    
+    # Return forms resulting from the discretization of the affine expansion of the problem operators.
+    def assemble_operator(self, term):
+        v = self.v
+        dx = self.dx
+        if term in ("a", "dc"):
+            u = self.u
+            a0 = inner(grad(u), grad(v))*dx(1)
+            a1 = inner(grad(u), grad(v))*dx(2)
+            return (a0, a1)
+        elif term == "c":
+            u = self._solution
+            c0 = inner(grad(u), grad(v))*dx(1)
+            c1 = inner(grad(u), grad(v))*dx(2)
+            return (c0, c1)
+        elif term == "f":
+            ds = self.ds
+            f0 = v*ds(1)
+            return (f0,)
+        elif term == "dirichlet_bc":
+            bc0 = [DirichletBC(self.V, Constant(0.0), self.boundaries, 3)]
+            return (bc0,)
+        elif term == "inner_product":
+            u = self.u
+            x0 = inner(grad(u), grad(v))*dx
+            return (x0,)
+        else:
+            raise ValueError("Invalid term for assemble_operator().")
+            
+# Customize the resulting reduced problem
+@CustomizeReducedProblemFor(NonlinearEllipticProblem)
+def CustomizeReducedNonlinearElliptic(ReducedNonlinearElliptic_Base):
+    class ReducedNonlinearElliptic(ReducedNonlinearElliptic_Base):
+        def __init__(self, truth_problem, **kwargs):
+            ReducedNonlinearElliptic_Base.__init__(self, truth_problem, **kwargs)
+            self._nonlinear_solver_parameters.update({
+                "report": True
+            })
+            
+    return ReducedNonlinearElliptic
+        
+# 1. Read the mesh for this problem
+mesh = Mesh("data/thermal_block.xml")
+subdomains = MeshFunction("size_t", mesh, "data/thermal_block_physical_region.xml")
+boundaries = MeshFunction("size_t", mesh, "data/thermal_block_facet_region.xml")
+
+# 2. Create Finite Element space (Lagrange P1)
+V = FunctionSpace(mesh, "Lagrange", 1)
+
+# 3. Allocate an object of the ThermalBlock class
+thermal_block_problem = ThermalBlock(V, subdomains=subdomains, boundaries=boundaries)
+mu_range = [(0.1, 10.0), (-1.0, 1.0)]
+thermal_block_problem.set_mu_range(mu_range)
+
+# 4. Prepare reduction with a reduced basis method
+pod_galerkin_method = PODGalerkin(thermal_block_problem)
+pod_galerkin_method.set_Nmax(4, DEIM=10)
+
+# 5. Perform the offline phase
+pod_galerkin_method.initialize_training_set(100, DEIM=100)
+reduced_thermal_block_problem = pod_galerkin_method.offline()
+
+# 6. Perform an online solve
+online_mu = (8.0, -1.0)
+reduced_thermal_block_problem.set_mu(online_mu)
+reduced_thermal_block_problem.solve()
+reduced_thermal_block_problem.export_solution(filename="online_solution")
+
+# 7. Perform an error analysis
+pod_galerkin_method.initialize_testing_set(100, DEIM=100)
+pod_galerkin_method.error_analysis()
+
+# 8. Perform a speedup analysis
+pod_galerkin_method.speedup_analysis()
