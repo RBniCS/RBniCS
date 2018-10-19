44eab1073bd986496442126705dd0d7f7820534a
Francesco Ballarin <francesco.ballarin@sissa.it>
2018-10-19 10:53:32 +0200

    Implement evaluation of stability factor for RB reduced problems

--- rbnics/scm/problems/decorated_reduced_problem_with_stability_factor_evaluation.py
+++ rbnics/scm/problems/decorated_reduced_problem_with_stability_factor_evaluation.py
@@ -16,25 +16,84 @@
 # along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
+from abc import abstractmethod
+from rbnics.scm.problems.parametrized_stability_factor_reduced_eigenproblem import ParametrizedStabilityFactorReducedEigenProblem
 from rbnics.utils.decorators import PreserveClassName
 
 def DecoratedReducedProblemWithStabilityFactorEvaluation(ParametrizedReducedDifferentialProblem_DerivedClass):
+    from rbnics.problems.elliptic_coercive import EllipticCoerciveRBReducedProblem
+    from rbnics.problems.stokes import StokesRBReducedProblem
     
     @PreserveClassName
-    class DecoratedReducedProblemWithStabilityFactorEvaluation_Class(ParametrizedReducedDifferentialProblem_DerivedClass):
+    class DecoratedReducedProblemWithStabilityFactorEvaluation_Class_Base(ParametrizedReducedDifferentialProblem_DerivedClass):
         # Default initialization of members
         def __init__(self, truth_problem, **kwargs):
+            # Eigen solver parameters (to be initialized before calling parent initialization as it may
+            # update them).
+            self._eigen_solver_parameters = {
+                "stability_factor": dict()
+            }
             # Call the parent initialization
             ParametrizedReducedDifferentialProblem_DerivedClass.__init__(self, truth_problem, **kwargs)
+            # Additional basis functions matrix required by stability factor computations
+            self.stability_factor_basis_functions = None # BasisFunctionsMatrix
+            # Stability factor eigen problem
+            self.stability_factor_calculator = ParametrizedStabilityFactorReducedEigenProblem(self, "smallest", self._eigen_solver_parameters["stability_factor"], os.path.join(truth_problem.name(), "reduced_stability_factor__NEVER_USED"))
+            
+        def init(self, current_stage="online"):
+            # Call parent initialization
+            ParametrizedReducedDifferentialProblem_DerivedClass.init(self, current_stage)
+            # Initialize attributes related to stability factor computations as well
+            self._init_stability_factor_basis_functions(current_stage)
+            self._init_stability_factor_calculator(current_stage)
+            
+        @abstractmethod
+        def _init_stability_factor_basis_functions(self, current_stage="online"):
+            pass
+            
+        def _init_stability_factor_calculator(self, current_stage="online"):
+            self.stability_factor_calculator.init()
             
         # Return the lower bound for the stability factor.
         def get_stability_factor_lower_bound(self, N=None, **kwargs):
             # Call the exact evaluation, since its computational cost is low because we are dealing with
             # left-hand and right-hand side matrices of small dimensions
             return self.evaluate_stability_factor()
-                
+        
         def evaluate_stability_factor(self, N=None, **kwargs):
-            raise NotImplementedError("Evaluation of reduced stability factor not implemented yet")
+            (minimum_eigenvalue, _) = self.stability_factor_calculator.solve()
+            return minimum_eigenvalue
+            
+    # Elliptic coercive RB reduced problem specialization
+    if issubclass(ParametrizedReducedDifferentialProblem_DerivedClass, EllipticCoerciveRBReducedProblem):
+        @PreserveClassName
+        class DecoratedReducedProblemWithStabilityFactorEvaluation_Class(DecoratedReducedProblemWithStabilityFactorEvaluation_Class_Base):
+            def __init__(self, truth_problem, **kwargs):
+                # Call the parent initialization
+                DecoratedReducedProblemWithStabilityFactorEvaluation_Class_Base.__init__(self, truth_problem, **kwargs)
+                # Update eigen solver parameters
+                self._eigen_solver_parameters["stability_factor"]["problem_type"] = "gen_hermitian"
+            
+            def _init_stability_factor_basis_functions(self, current_stage="online"):
+                self.stability_factor_basis_functions = self.basis_functions
+                
+    # Stokes RB reduced problem specialization
+    elif issubclass(ParametrizedReducedDifferentialProblem_DerivedClass, StokesRBReducedProblem):
+        @PreserveClassName
+        class DecoratedReducedProblemWithStabilityFactorEvaluation_Class(DecoratedReducedProblemWithStabilityFactorEvaluation_Class_Base):
+            def __init__(self, truth_problem, **kwargs):
+                # Call the parent initialization
+                DecoratedReducedProblemWithStabilityFactorEvaluation_Class_Base.__init__(self, truth_problem, **kwargs)
+                # Update eigen solver parameters
+                self._eigen_solver_parameters["stability_factor"]["problem_type"] = "pos_gen_non_hermitian"
+                
+            def _init_stability_factor_basis_functions(self, current_stage="online"):
+                self.stability_factor_basis_functions = self.basis_functions
+                
+    # Unhandled case: will return an error due to abstract methods
+    else:
+        DecoratedReducedProblemWithStabilityFactorEvaluation_Class = DecoratedReducedProblemWithStabilityFactorEvaluation_Class_Base
         
     # return value (a class) for the decorator
     return DecoratedReducedProblemWithStabilityFactorEvaluation_Class
---
+++ rbnics/scm/problems/parametrized_stability_factor_reduced_eigenproblem.py
@@ -0,0 +1,107 @@
+# Copyright (C) 2015-2018 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# RBniCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RBniCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with RBniCS. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from numpy import isclose
+from rbnics.problems.base import ParametrizedProblem
+from rbnics.backends import assign, copy, product, sum
+from rbnics.backends.online import OnlineEigenSolver, OnlineFunction
+from rbnics.utils.cache import Cache
+from rbnics.utils.decorators import sync_setters
+
+class ParametrizedStabilityFactorReducedEigenProblem(ParametrizedProblem):
+
+    # Default initialization of members
+    @sync_setters("reduced_problem", "set_mu", "mu")
+    @sync_setters("reduced_problem", "set_mu_range", "mu_range")
+    def __init__(self, reduced_problem, spectrum, eigensolver_parameters, folder_prefix):
+        # Call the parent initialization
+        ParametrizedProblem.__init__(self, folder_prefix) # this class does not export anything
+        self.reduced_problem = reduced_problem
+        
+        # Matrices/vectors resulting from the truth discretization
+        self.operator = {
+            "stability_factor_left_hand_matrix": None, # OnlineAffineExpansionStorage
+            "stability_factor_right_hand_matrix": None # OnlineAffineExpansionStorage, even though it will contain only one matrix
+        }
+        self.spectrum = spectrum
+        self.eigensolver_parameters = eigensolver_parameters
+        
+        # Solution
+        self._eigenvalue = 0.
+        self._eigenvector = None # OnlineFunction
+        # I/O
+        def _eigenvalue_cache_key_generator(*args, **kwargs):
+            return args
+        self._eigenvalue_cache = Cache(
+            "reduced problems",
+            key_generator=_eigenvalue_cache_key_generator
+        )
+        def _eigenvector_cache_key_generator(*args, **kwargs):
+            return args
+        self._eigenvector_cache = Cache(
+            "reduced problems",
+            key_generator=_eigenvector_cache_key_generator
+        )
+    
+    def init(self, current_stage="online"):
+        # Store the left and right hand side operators
+        if self.operator["stability_factor_left_hand_matrix"] is None: # init was not called already
+            self.operator["stability_factor_left_hand_matrix"] = self.reduced_problem.operator["stability_factor_left_hand_matrix"]
+        if self.operator["stability_factor_right_hand_matrix"] is None: # init was not called already
+            self.operator["stability_factor_right_hand_matrix"] = self.reduced_problem.operator["stability_factor_right_hand_matrix"]
+            assert len(self.operator["stability_factor_right_hand_matrix"]) is 1
+            
+    def solve(self, N=None, **kwargs):
+        N, kwargs = self.reduced_problem._online_size_from_kwargs(N, **kwargs)
+        N += self.reduced_problem.N_bc
+        self._eigenvector = OnlineFunction(N)
+        cache_key = self._cache_key(**kwargs)
+        try:
+            self._eigenvalue = self._eigenvalue_cache[cache_key]
+            assign(self._eigenvector, self._eigenvector_cache[cache_key])
+        except KeyError:
+            self._solve(N, **kwargs)
+            self._eigenvalue_cache[cache_key] = self._eigenvalue
+            self._eigenvector_cache[cache_key] = copy(self._eigenvector)
+        return (self._eigenvalue, self._eigenvector)
+        
+    def _solve(self, N, **kwargs):
+        assert self.operator["stability_factor_left_hand_matrix"] is not None
+        A = sum(product(self.reduced_problem.compute_theta("stability_factor_left_hand_matrix"), self.operator["stability_factor_left_hand_matrix"][:N, :N]))
+        assert self.operator["stability_factor_right_hand_matrix"] is not None
+        assert len(self.operator["stability_factor_right_hand_matrix"]) == 1
+        B = self.operator["stability_factor_right_hand_matrix"][0][:N, :N]
+        
+        eigensolver = OnlineEigenSolver(self.reduced_problem.stability_factor_basis_functions, A, B)
+        eigensolver_parameters = dict()
+        assert self.spectrum is "largest" or self.spectrum is "smallest"
+        eigensolver_parameters["spectrum"] = self.spectrum + " real"
+        eigensolver_parameters.update(self.eigensolver_parameters)
+        eigensolver.set_parameters(eigensolver_parameters)
+        eigensolver.solve(1)
+        
+        r, c = eigensolver.get_eigenvalue(0) # real and complex part of the eigenvalue
+        r_vector, c_vector = eigensolver.get_eigenvector(0) # real and complex part of the eigenvectors
+        
+        assert isclose(c, 0.), "The required eigenvalue is not real"
+        
+        self._eigenvalue = r
+        assign(self._eigenvector, r_vector)
+        
+    def _cache_key(self, **kwargs):
+        return (self.mu, self.spectrum, tuple(sorted(kwargs.items())))
