b3632576a1314578180b8e0defbcaac7c0fb2f69
Leonardo Scandurra <lscandur@sissa.it>
2021-04-12 17:50:29 +0200

    Implement output of interest for the nonlinear elliptic problems

--- rbnics/problems/nonlinear_elliptic/nonlinear_elliptic_reduced_problem.py
+++ rbnics/problems/nonlinear_elliptic/nonlinear_elliptic_reduced_problem.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 from rbnics.problems.base import NonlinearReducedProblem
-from rbnics.backends import product, sum
+from rbnics.backends import product, sum, transpose
 
 
 def NonlinearEllipticReducedProblem(EllipticReducedProblem_DerivedClass):
@@ -31,6 +31,10 @@ def NonlinearEllipticReducedProblem(EllipticReducedProblem_DerivedClass):
                 assembled_operator["a"] = sum(product(problem.compute_theta("a"), problem.operator["a"][:N, :N]))
                 assembled_operator["dc"] = sum(product(problem.compute_theta("dc"), problem.operator["dc"][:N, :N]))
                 return assembled_operator["a"] + assembled_operator["dc"]
+
+        # Perform an online evaluation of the output
+        def _compute_output(self, N):
+            self._output = transpose(self._solution) * sum(product(self.compute_theta("s"), self.operator["s"][:N]))
 
     # return value (a class) for the decorator
     return NonlinearEllipticReducedProblem_Class
